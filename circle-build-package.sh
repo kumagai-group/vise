15e741a442204f478f37f42986dc8bef53d17c8a
kuma <yuuukuma@gmail.com>
2020-03-02 20:21:15 +0900

    Modify circleCI config and change version to 0.1.5

--- circle-build-package.sh
+++ circle-build-package.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-if [ -z "$CI" ]; then
-    echo "Will only continue on CI"
-    exit
-fi
+#if [ -z "$CI" ]; then
+#    echo "Will only continue on CI"
+#    exit
+#fi
 
-if [[ $CIRCLE_BRANCH != "master" ]]; then
-    echo "Will only continue for master builds"
-    exit
-fi
+#if [[ $CIRCLE_BRANCH != "master" ]]; then
+#    echo "Will only continue for master builds"
+#    exit
+#fi
 
 # build package and upload to private pypi index
 touch ~/.pypirc
@@ -16,9 +16,14 @@ touch ~/.pypirc
   echo "[distutils]"
   echo "index-servers = pypi-private"
   echo "[pypi-private]"
-  echo "repository=https://$PYPI_HOST"
-  echo "username=$PYPI_USERNAME"
-  echo "password=$PYPI_PASSWORD"
+  echo "repository=https://test.pypi.org/vise/"
+  echo "username=yuuukuma"
+  echo "password=48a7a59"
 } >> ~/.pypirc
 
-python setup.py sdist upload -r pypi-private
+#python setup.py sdist upload -r pypi-private
+twine upload --repository pypi-private dist/*
+
+#  echo "repository=https://$PYPI_HOST"
+#  echo "username=$PYPI_USERNAME"
+#  echo "password=$PYPI_PASSWORD"
