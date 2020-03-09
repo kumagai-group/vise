cd5abcb1f134a46261d965949465242c48d4af3d
yuuukuma <yuuukuma@gmail.com>
2020-03-09 21:10:51 +0900

    Add test of band_structure for ferromagnetic system.

--- vise/analyzer/tests/test_band_plotter.py
+++ vise/analyzer/tests/test_band_plotter.py
@@ -6,12 +6,11 @@ from vise.util.testing import ViseTest
 from vise.analyzer.band_plotter import (
     PrettyBSPlotter, ModBSPlotter, VaspBandStructureSymmLine)
 
+parent_dir = Path(__file__).parent
 
-class PlotBandStructureTest(ViseTest):
+
+class PlotTwoBandStructureTest(ViseTest):
     def setUp(self) -> None:
-
-        parent_dir = Path(__file__).parent
-
         self.mgo_band = VaspBandStructureSymmLine(
             parent_dir / "MgO_band_KPOINTS",
             parent_dir / "MgO_band_vasprun.xml", is_projection=True)
@@ -22,5 +21,17 @@ class PlotBandStructureTest(ViseTest):
     def test(self):
         plotter = PrettyBSPlotter(band=self.mgo_band, band2=self.mgo_nhse_band,
                                   absolute=False)
+        plotter.show()
+
+
+class PlotFerromagneticBandStructureTest(ViseTest):
+    def setUp(self) -> None:
+        self.ko2_band = VaspBandStructureSymmLine(
+            kpoints_filename=(parent_dir / "KO2_band_KPOINTS"),
+            vasprun_filename=(parent_dir / "KO2_band_vasprun.xml"),
+            is_projection=True)
+
+    def test(self):
+        plotter = PrettyBSPlotter(band=self.ko2_band, absolute=False)
         plotter.show()
 
