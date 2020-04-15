2a25ba29309c8fa7b8979a3aade1e95261f67863
yuuukuma <yuuukuma@gmail.com>
2020-04-15 18:43:27 +0900

    Remove element_parameters.py

--- vise/input_set/datasets/element_parameters.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from pathlib import Path
-
-from monty.serialization import loadfn
-
-unoccupied_bands = loadfn(Path(__file__).parent / "unoccupied_bands.yaml")
-magmom = loadfn(Path(__file__).parent / "magmom.yaml")
