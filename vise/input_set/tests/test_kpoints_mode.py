28773c957eda5afb013f5b2a17db8d10ce5eaa56
yuuukuma <yuuukuma@gmail.com>
2020-04-17 08:33:55 +0900

    Move KpointsMode to kpoints_mode.py
    Move its unittests to test_kpoints_mode.py, too.

---
+++ vise/input_set/tests/test_kpoints_mode.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+
+import pytest
+from vise.input_set.kpoints_mode import KpointsMode
+
+
+def test_kpoints_mode_from_string():
+    assert KpointsMode.from_string("band") == KpointsMode.band
+
+
+def test_kpoints_mode_from_string_raise_error():
+    with pytest.raises(AttributeError):
+        KpointsMode.from_string("fail_string")
+
+
+def test_kpoints_mode_name_list():
+    expected = "band, primitive, uniform"
+    assert KpointsMode.name_list() == expected
