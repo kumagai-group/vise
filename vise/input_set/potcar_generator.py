e779fe17bf90a59bc69d0c760aceb3204a287ec8
yuuukuma <yuuukuma@gmail.com>
2020-04-17 11:34:45 +0900

    Modify generate_potcar to use PotcarSet

--- vise/input_set/potcar_generator.py
+++ vise/input_set/potcar_generator.py
@@ -5,7 +5,7 @@ from typing import Optional
 
 from pymatgen.io.vasp.sets import Potcar
 
-from vise.input_set.settings_util import load_potcar_yaml
+from vise.input_set.datasets.dataset_util import PotcarSet
 from vise.input_set.xc import Xc
 from vise.util.logger import get_logger
 
@@ -14,11 +14,11 @@ logger = get_logger(__name__)
 
 def generate_potcar(symbol_list: list,
                     xc: Xc = Xc.pbe,
-                    potcar_set_name: str = "normal",
+                    potcar_set: PotcarSet = PotcarSet.normal,
                     override_potcar_set: Optional[dict] = None):
 
-    potcar_list = load_potcar_yaml(potcar_set_name, override_potcar_set)
-    potcar_symbols = [potcar_list.get(el, el) for el in symbol_list]
+    potcar_dict = potcar_set.potcar_dict(override_potcar_set)
+    potcar_symbols = [potcar_dict[el] for el in symbol_list]
     try:
         return Potcar(potcar_symbols, functional=xc.potcar_functional)
     except IOError as e:
