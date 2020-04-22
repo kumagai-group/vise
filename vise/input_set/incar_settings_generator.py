5a0d62e235523411b8891a538ee11755de9d1d1b
yuuukuma <yuuukuma@gmail.com>
2020-04-22 15:26:24 +0900

    Refactor IncarSettingsGenerator

--- vise/input_set/incar_settings_generator.py
+++ vise/input_set/incar_settings_generator.py
@@ -7,7 +7,7 @@ from typing import Optional, Union, List, Dict
 from pymatgen import Composition
 from pymatgen.io.vasp.sets import Potcar
 
-from vise.config import (
+from vise.defaults import (
     DOS_STEP_SIZE, ENCUT_FACTOR_STR_OPT, DEFAULT_NUM_NODES,
     BAND_GAP_CRITERION)
 from vise.input_set.datasets.dataset_util import num_bands, npar_kpar, LDAU
@@ -26,8 +26,8 @@ class IncarSettingsGenerator:
             num_kpts: int,
             num_kpt_multiplication_factor: int,
             potcar: Potcar,
-            xc: Xc,
             task: Task,
+            xc: Xc,
             dos_step_size: float = DOS_STEP_SIZE,  # in eV
             charge: float = 0.0,  # in elementary charge as in vasp
             vbm_cbm: Optional[List[float]] = None,  # [vbm, cbm] in absolute eV
@@ -46,8 +46,8 @@ class IncarSettingsGenerator:
         self._num_kpt_multiplication_factor = num_kpt_multiplication_factor
         self._potcar = potcar
         self._num_kpts = num_kpts
-        self._xc = xc
         self._task = task
+        self._xc = xc
         self._dos_step_size = dos_step_size
         self._charge = charge
         self._vbm_cbm = vbm_cbm
@@ -60,56 +60,72 @@ class IncarSettingsGenerator:
         self._num_nodes = num_nodes
         self._str_opt_encut_multi_factor = str_opt_encut_multi_factor
 
-        # remove keys with None values
-        self._settings = {
-            "ALGO": self._xc.incar_algo,
+        self._incar_settings = {}
+        self._set_default_settings()
+        self._set_task_related_settings()
+        self._set_xc_related_settings()
+        self._set_multiple_conditions_related_settings()
 
+        if self._task.is_spectrum_task:
+            self._set_spectrum_related_settings()
+        if self._task.is_dielectric_task:
+            self._set_dielectric_related_settings()
+        if self._xc.is_hybrid_functional:
+            self._set_hybrid_func_related_settings()
+        if self._need_hubbard_u(set_hubbard_u):
+            self._set_hubbard_u_related_settings()
+        if self._auto_npar_kpar:
+            self._set_npar_kpar()
+
+        self._remove_incar_settings_with_none_values()
+
+    def _set_default_settings(self):
+        self._incar_settings.update({
+            "LASPH": True,
+            "NELM": 100,
+            "SIGMA": 0.1,
+            "LCHARG": False,
+            "LORBIT": 12,
+        })
+
+    def _set_task_related_settings(self):
+        self._incar_settings.update({
             "PREC": self._task.incar_prec,
             "LREAL": self._task.incar_lreal,
             "EDIFF": self._task.incar_ediff,
-            "ENCUT": self._encut,
             "ADDGRID": self._task.incar_addgrid_optional,
-            "LASPH": True,
-            "NELM": 100,
-
             "ISIF": self._task.incar_isif,
             "IBRION": self._task.incar_ibrion,
             "EDIFFG": self._task.incar_ediffg_optional,
             "NSW": self._task.incar_nsw,
             "POTIM": self._task.incar_potim_optional,
+        })
 
-            "ISMEAR": self._ismear,
-            "SIGMA": 0.1,
-
-            "ISPIN": self._ispin,
-
+    def _set_xc_related_settings(self):
+        self._incar_settings.update({
+            "ALGO": self._xc.incar_algo,
             "LWAVE": self._xc.incar_lwave,
-            "LCHARG": False,
-
-            "NBANDS": self._nbands,
-            "LORBIT": 12,
-
             "GGA": self._xc.incar_gga_optional,
             "METAGGA": self._xc.incar_metagga_optional,
+        })
 
+    def _set_multiple_conditions_related_settings(self):
+        self._incar_settings.update({
+            "ENCUT": self._encut,
+            "ISMEAR": self._ismear,
+            "ISPIN": self._ispin,
+            "NBANDS": self._nbands,
             "NELECT": self._nelect,
-        }
-
-        if self._task.is_spectrum_task:
-            self._set_spectrum_related_tags()
-        if self._task.is_dielectric_task:
-            self._set_dielectric_related_tags()
-        if self._xc.is_hybrid_functional:
-            self._set_hybrid_func_related_tags()
-        if self._need_hubbard_u(set_hubbard_u):
-            self._set_hubbard_u_related_tags()
-        self._set_npar_kpar()
-        self._remove_tags_with_none_values()
+        })
 
     def _need_hubbard_u(self, set_hubbard_u):
         if type(set_hubbard_u) == bool:
             return set_hubbard_u
-        return (self._ldauu is not None) or self._xc.is_lda_or_gga
+        if self._ldauu is not None:
+            return True
+        if self._xc.is_lda_or_gga:
+            return True
+        return False
 
     @property
     def _encut(self) -> float:
@@ -122,11 +138,9 @@ class IncarSettingsGenerator:
         return round(max_enmax, 3)
 
     @property
-    def _ismear(self) -> Union[int, None]:
+    def _ismear(self) -> int:
         # Tetrahedron method fails for irrep. NKPT<4 in vasp.
-        if (is_band_gap(self._vbm_cbm)
-                and self._num_kpts >= 4
-                and self._task.is_spectrum_task):
+        if is_band_gap(self._vbm_cbm) and self._num_kpts >= 4:
             return -5
         return 0
 
@@ -150,64 +164,63 @@ class IncarSettingsGenerator:
             return nelect - self._charge
         return
 
-    def _set_spectrum_related_tags(self) -> None:
+    def _set_spectrum_related_settings(self) -> None:
         if self._vbm_cbm:
             emin = ceil(self._vbm_cbm[0]) - 15 - self._dos_step_size
             emax = ceil(self._vbm_cbm[1]) + 15
         else:
             emin = -20 - self._dos_step_size
             emax = 20
-        self._settings["EMIN"] = emin
-        self._settings["EMAX"] = emax
+        self._incar_settings["EMIN"] = emin
+        self._incar_settings["EMAX"] = emax
         num_steps = round((emax - emin) / self._dos_step_size)
-        self._settings["NEDOS"] = num_steps + 1
+        self._incar_settings["NEDOS"] = num_steps + 1
 
-    def _set_dielectric_related_tags(self) -> None:
+    def _set_dielectric_related_settings(self) -> None:
         if self._task == Task.dielectric_dfpt:
-            self._settings["LEPSILON"] = True
+            self._incar_settings["LEPSILON"] = True
         elif self._task == Task.dielectric_finite_field:
-            self._settings["LCALCEPS"] = True
-            self._settings["IBRION"] = 6
-            self._settings["POTIM"] = 0.015
+            self._incar_settings["LCALCEPS"] = True
+            self._incar_settings["IBRION"] = 6
+            self._incar_settings["POTIM"] = 0.015
         elif self._task == Task.dielectric_function:
-            self._settings["LOPTICS"] = True
-            self._settings["CSHIFT"] = 0.01
+            self._incar_settings["LOPTICS"] = True
+            self._incar_settings["CSHIFT"] = 0.01
 
-    def _set_hybrid_func_related_tags(self) -> None:
-        self._settings["LHFCALC"] = True
-        self._settings["PRECFOCK"] = "Fast"
-        self._settings["TIME"] = 0.4
-        self._settings["AEXX"] = self._aexx
+    def _set_hybrid_func_related_settings(self) -> None:
+        self._incar_settings["LHFCALC"] = True
+        self._incar_settings["PRECFOCK"] = "Fast"
+        self._incar_settings["TIME"] = 0.4
+        self._incar_settings["AEXX"] = self._aexx
         if self._num_kpt_multiplication_factor != 1:
-            self._settings["NKRED"] = self._num_kpt_multiplication_factor
+            self._incar_settings["NKRED"] = self._num_kpt_multiplication_factor
         if self._xc is Xc.hse:
-            self._settings["HFSCREEN"] = 0.208
+            self._incar_settings["HFSCREEN"] = 0.208
 
-    def _set_hubbard_u_related_tags(self) -> None:
+    def _set_hubbard_u_related_settings(self) -> None:
         ldau = LDAU(self._symbol_list, self._ldauu, self._ldaul)
         if ldau.is_ldau_needed:
-            self._settings["LDAU"] = True
-            self._settings["LDAUTYPE"] = 2
-            self._settings["LMAXMIX"] = ldau.lmaxmix
-            self._settings["LDAUPRINT"] = 1
-            self._settings["LDAUU"] = ldau.ldauu
-            self._settings["LDAUL"] = ldau.ldaul
+            self._incar_settings["LDAU"] = True
+            self._incar_settings["LDAUTYPE"] = 2
+            self._incar_settings["LMAXMIX"] = ldau.lmaxmix
+            self._incar_settings["LDAUPRINT"] = 1
+            self._incar_settings["LDAUU"] = ldau.ldauu
+            self._incar_settings["LDAUL"] = ldau.ldaul
 
     def _set_npar_kpar(self) -> None:
-        if self._auto_npar_kpar:
-            kpar, npar = npar_kpar(self._num_kpts, self._num_nodes)
-            self._settings["KPAR"] = kpar
-            # now switch off NPAR
-            # self._settings["NPAR"] = npar
+        kpar, npar = npar_kpar(self._num_kpts, self._num_nodes)
+        self._incar_settings["KPAR"] = kpar
+        # now switch off NPAR
+        # self._settings["NPAR"] = npar
 
-    def _remove_tags_with_none_values(self) -> None:
-        for tag_name, value in self._settings.copy().items():
+    def _remove_incar_settings_with_none_values(self) -> None:
+        for tag_name, value in self._incar_settings.copy().items():
             if value is None:
-                self._settings.pop(tag_name)
+                self._incar_settings.pop(tag_name)
 
     @property
     def incar_settings(self) -> Dict[str, Union[str, bool, int, float]]:
-        return self._settings
+        return self._incar_settings
 
 
 def is_band_gap(vbm_cbm: Optional[List[float]]) -> bool:
