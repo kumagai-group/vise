80837d427cc4f182d2bb7c1d770a5ba41b30e695
yuuukuma <yuuukuma@gmail.com>
2020-04-24 08:04:55 +0900

    Rename some arguments and some methods

--- vise/input_set/input_options.py
+++ vise/input_set/input_options.py
@@ -32,7 +32,7 @@ class DividedInputOptions:
         self._raise_error_when_unknown_options_exist()
 
     def _raise_error_when_unknown_options_exist(self) -> None:
-        unknown_args_set = self.input_option_set - self.all_option_set
+        unknown_args_set = self.input_option_set - self.assignable_option_set
         if unknown_args_set:
             raise ViseInputOptionsError(
                 f"Options {unknown_args_set} are invalid")
@@ -42,7 +42,7 @@ class DividedInputOptions:
         return set(self._input_options.keys())
 
     @property
-    def all_option_set(self) -> set:
+    def assignable_option_set(self) -> set:
         return (set(self.structure_kpoints_args)
                 | set(self.potcar_args)
                 | set(self.incar_settings_args))
@@ -63,33 +63,33 @@ class DividedInputOptions:
     def function_args(func) -> List[str]:
         return getfullargspec(func).args
 
-    def constructor_args(self, target_cls) -> List[str]:
-        result = self.function_args(target_cls.__init__)
+    def constructor_args(self, target_class) -> List[str]:
+        result = self.function_args(target_class.__init__)
         result.remove("self")
+        return result
+
+    def pick_target_options(self, target_args: List[str]) -> Dict[str, Any]:
+        result = {}
+        for target_arg in target_args:
+            if target_arg in self._input_options:
+                result[target_arg] = self._input_options[target_arg]
         return result
 
     @property
     def structure_kpoints_options(self) -> Dict[str, Any]:
-        return self.pick_target_options(self.structure_kpoints_args)
+        return self.pick_target_options(target_args=self.structure_kpoints_args)
 
     @property
     def potcar_options(self) -> Dict[str, Any]:
-        return self.pick_target_options(self.potcar_args)
+        return self.pick_target_options(target_args=self.potcar_args)
 
     @property
     def incar_settings_options(self) -> Dict[str, Any]:
-        return self.pick_target_options(self.incar_settings_args)
+        return self.pick_target_options(target_args=self.incar_settings_args)
 
     @property
     def initial_structure(self) -> Structure:
         return self._input_options["initial_structure"]
-
-    def pick_target_options(self, target_args: List[str]) -> Dict[str, Any]:
-        result = {}
-        for option in target_args:
-            if option in self._input_options:
-                result[option] = self._input_options[option]
-        return result
 
 
 class ViseInputOptionsError(KeyError):
