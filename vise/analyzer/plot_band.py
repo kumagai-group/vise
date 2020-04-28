1ea835f5069a223693aa19868e958a12f7194ad0
yuuukuma <yuuukuma@gmail.com>
2020-04-28 16:13:44 +0900

    Update plot_band and its unittests

--- vise/analyzer/plot_band.py
+++ vise/analyzer/plot_band.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 
-from itertools import tee, groupby
+from itertools import tee
 import re
 from typing import List, Dict, Optional
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
 
 from pymatgen import Spin
+from pymatgen.electronic_structure.plotter import BSPlotter
+from pymatgen.io.vasp import Vasprun
+
 from vise.util.matplotlib import float_to_int_formatter
 
 
 @dataclass()
-class XTics:
-    # labels include "x|y"
+class XTicks:
+    # labels include "x$\\mid$y"
     labels: List[str]
     distances: List[float]
 
@@ -28,38 +31,53 @@ class BandEdge:
 
 @dataclass
 class BandInfo:
-    band_energies: Dict[Spin, List[List[float]]]
+    band_energies_by_branch: List[Dict[Spin, List[List[float]]]]
     band_edge: Optional[BandEdge] = None
     fermi_level: Optional[float] = None
-    band_structure_args: Optional[dict] = None
-    band_edge_args: Optional[dict] = None
 
-    def __post_init__(self):
-        if self.band_structure_args is None:
-            self.band_structure_args = \
-                {"linestyle": "-", "color": "red", "linewidth": 1}
-        if self.band_edge_args is None:
-            self.band_edge_args = {"color": "r", "marker": "o", "s": 100}
+
+class BandPlotterDefaults:
+    def __init__(self,
+                 band_linestyle: Optional[List[str]] = None,
+                 band_colors: Optional[List[str]] = None,
+                 band_linewidth: float = 1.0,
+                 band_edge_circle_size: int = 100
+                 ):
+        self.band_linestyle = band_linestyle or ["-"]
+        self.band_colors = band_colors or ["red", "blue"]
+        self.band_linewidth = band_linewidth
+        self.band_edge_circle_size = band_edge_circle_size
+
+    @property
+    def band_args(self):
+        return {"linestyle": self.band_linestyle[0],
+                "color": self.band_colors[0],
+                "linewidth": self.band_linewidth}
+
+    @property
+    def band_edge_circles_args(self):
+        return {"marker": "o",
+                "s": self.band_edge_circle_size}
 
 
 class BandPlotter:
 
     def __init__(self,
                  band_info_set: List[BandInfo],
-                 distances: List[float],
-                 x_ticks: XTics,
+                 distances_by_branch: List[List[float]],
+                 x_ticks: XTicks,
                  y_range: List[float],
-                 **frame_args
+                 defaults: Optional[BandPlotterDefaults] = BandPlotterDefaults()
                  ):
 
-        assert distances[0] == x_ticks.distances[0]
-        assert distances[-1] == x_ticks.distances[-1]
+        assert distances_by_branch[0][0] == x_ticks.distances[0]
+        assert distances_by_branch[-1][-1] == x_ticks.distances[-1]
 
-        self._band_info_set = band_info_set
-        self._distances = distances
-        self._x_ticks = x_ticks
-        self._y_range = y_range
-        self._set_plot_args(frame_args)
+        self.band_info_set = band_info_set
+        self.distances_by_branch = distances_by_branch
+        self.x_ticks = x_ticks
+        self.y_range = y_range
+        self.defaults = defaults
         self.plt = plt
 
     def construct_plot(self):
@@ -71,47 +89,43 @@ class BandPlotter:
         self._set_formatter()
         self.plt.tight_layout()
 
-    def _set_plot_args(self, input_args):
-        # self.plot_args = {
-        #     "band_structure": {"linestyle": "-", "color": "red", "linewidth": 1},
-        #     "band_edge":
-        # }
-#        self.frame_args
-#        self.plot_args.update(input_args)
-        pass
-
     def _set_band_set(self):
-        for band_info in self._band_info_set:
-            self._set_band_structures(band_info)
+        for band_info in self.band_info_set:
+            self._set_band_structures(band_info.band_energies_by_branch)
             if band_info.band_edge:
-                self._set_band_edge(band_info.band_edge,
-                                    **band_info.band_edge_args)
+                self._set_band_edge(band_info.band_edge)
             if band_info.fermi_level:
                 self._set_fermi_level(band_info.fermi_level)
 
-    def _set_band_structures(self, band_info):
-        for spin, band_energies in band_info.band_energies.items():
-            for energies_per_band in band_energies:
-                self.plt.plot(self._distances, energies_per_band,
-                              **band_info.band_structure_args)
+    def _set_band_structures(self, energies_by_branch):
+        for distances_of_a_branch, energies_of_a_branch \
+                in zip(self.distances_by_branch, energies_by_branch):
 
-    def _set_band_edge(self, band_edge, **plot_args):
+            for spin, energies_of_a_spin in energies_of_a_branch.items():
+                for energies_of_a_band in energies_of_a_spin:
+                    self.plt.plot(distances_of_a_branch, energies_of_a_band,
+                                  **self.defaults.band_args)
+
+    def _set_band_edge(self, band_edge):
         self.plt.axhline(y=band_edge.vbm)
         for dist in band_edge.vbm_distances:
-            self.plt.scatter(dist, band_edge.vbm, **plot_args)
+            self.plt.scatter(dist, band_edge.vbm,
+                             **self.defaults.band_edge_circles_args)
 
         self.plt.axhline(y=band_edge.cbm)
         for dist in band_edge.cbm_distances:
-            self.plt.scatter(dist, band_edge.cbm, **plot_args)
+            self.plt.scatter(dist, band_edge.cbm,
+                             **self.defaults.band_edge_circles_args)
 
     def _set_fermi_level(self, fermi_level):
         self.plt.axhline(y=fermi_level)
 
     def _set_x_range(self):
-        self.plt.xlim(self._distances[0], self._distances[-1])
+        self.plt.xlim(self.distances_by_branch[0][0],
+                      self.distances_by_branch[-1][-1])
 
     def _set_y_range(self):
-        self.plt.ylim(self._y_range[0], self._y_range[1])
+        self.plt.ylim(self.y_range[0], self.y_range[1])
 
     def _set_axes(self):
         self.plt.xlabel("Wave vector")
@@ -119,25 +133,16 @@ class BandPlotter:
 
     def _set_xticks(self):
         axis = self.plt.gca()
-        axis.set_xticks(self._x_ticks.distances)
-        axis.set_xticklabels(self._x_ticks.labels)
-        for distance, label in zip(self._x_ticks.distances[1:-1],
-                                   self._x_ticks.labels[1:-1]):
-            if "|" in label:
-                plt.axvline(x=distance)
-            else:
-                plt.axvline(x=distance, linestyle="--")
+        axis.set_xticks(self.x_ticks.distances)
+        axis.set_xticklabels(self.x_ticks.labels)
+        for distance, label in zip(self.x_ticks.distances[1:-1],
+                                   self.x_ticks.labels[1:-1]):
+            linestyle = "-" if "\\mid" in label else "--"
+            plt.axvline(x=distance, linestyle=linestyle)
 
     def _set_formatter(self):
         axis = self.plt.gca()
-        axis.xaxis.set_major_formatter(float_to_int_formatter)
         axis.yaxis.set_major_formatter(float_to_int_formatter)
-
-
-def pairwise(iterable):
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
 
 
 def greek_to_unicode(label: str) -> str:
@@ -148,29 +153,40 @@ def greek_to_unicode(label: str) -> str:
 
 
 def italic_to_roman(label: str) -> str:
-    return re.sub(r"([A-Z])_([0-9])", r"${\\rm \1}_\2$", label)
+    return re.sub(r"([A-Z])_([0-9])", r"{\\rm \1}_\2", label)
 
 
+class VaspBandPlotter(BandPlotter):
+    def __init__(self, vasprun: Vasprun, kpoints_filename: str):
 
-# class Xtics:
-#     def __init__(self, labels: List[str], distances: List[float]):
-#         assert all(prior <= posterior
-#                    for prior, posterior in pairwise(distances))
-#
-#         self.distances = []
-#         self.labels = []
-#         self.vertical_line_positions = []
-#         distances_labels = list(zip(distances, labels))
-#         for distance, group in groupby(distances_labels, key=lambda x: x[0]):
-#             self.distances.append(distance)
-#             labels = [g[1] for g in group]
-#             label = "|".join(labels)
-#             self.labels.append(label)
-#             if len(labels) > 1:
-#                 self.vertical_line_positions.append(distance)
-#
-#     @staticmethod
-#     def sanitize_label(label):
-#         return italic_to_roman(greek_to_unicode(label))
+        self.bs = vasprun.get_band_structure(kpoints_filename, line_mode=True)
+        self.plot_data = BSPlotter(self.bs).bs_plot_data(zero_to_efermi=False)
 
+        band_info = BandInfo(band_energies_by_branch=self.plot_data["energy"],
+                             band_edge=self._band_edge,
+                             fermi_level=self.bs.efermi)
 
+        super().__init__(band_info_set=[band_info],
+                         distances_by_branch=self.plot_data["distances"],
+                         x_ticks=self._x_ticks,
+                         y_range=[-10, 10])
+
+    @property
+    def _x_ticks(self):
+        labels = self._sanitize_labels(self.plot_data["ticks"]["label"])
+        distances = self.plot_data["ticks"]["distance"]
+        x_ticks = XTicks(labels=labels, distances=distances)
+        return x_ticks
+
+    @property
+    def _band_edge(self):
+        if self.bs.is_metal():
+            return None
+        return BandEdge(vbm=self.plot_data["vbm"][0][1],
+                        cbm=self.plot_data["cbm"][0][1],
+                        vbm_distances=[i[0] for i in self.plot_data["vbm"]],
+                        cbm_distances=[i[0] for i in self.plot_data["cbm"]])
+
+    @staticmethod
+    def _sanitize_labels(label_list):
+        return [italic_to_roman(greek_to_unicode(l)) for l in label_list]
--- vise/tests/analyzer/test_plot_band.py
+++ vise/tests/analyzer/test_plot_band.py
@@ -2,22 +2,27 @@
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 
 import pytest
+from pathlib import Path
 
 from vise.analyzer.plot_band import (
-    greek_to_unicode, italic_to_roman, pairwise, BandPlotter, BandInfo, BandEdge, XTics
-)
+    greek_to_unicode, italic_to_roman, BandPlotter, BandInfo,
+    BandEdge, XTicks, BandPlotterDefaults,
+    VaspBandPlotter)
 from unittest.mock import MagicMock
 
 from pymatgen import Spin
 from vise.util.matplotlib import float_to_int_formatter
+from pymatgen.electronic_structure.bandstructure import BandStructureSymmLine
+from pymatgen.io.vasp import Vasprun, Outcar
 
 """
 TODO:
-+ Set tuning parameters
++ From a vasprun.xml and a KPOINTS, generate minimum BandPlotter data.
 
-+ Enable multiple bands
-+ Create VaspBandPlotter inheriting BandPlotter
-+ Move a|b part to VaspBandPlotter
++ Generate cyclic color and linestyle for band structures.
++ Enable multiple bands (distances and x_ticks must be the same)
++ Consider if the structure is parsed or not.
++
 
 DONE:
 + Draw a non-magnetic band with a single band
@@ -33,19 +38,23 @@ DONE:
 + Set horizontal lines (vbm, cbm)
 + Add dashed line for the positions with labels
 + Use set_major_formatter
++ Create a class that holds default values for drawing band structures.
++ Set tuning parameters with BandPlotterDefaults class
++ Create VaspBandPlotter inheriting BandPlotter
++ Move a|b part to VaspBandPlotter
 """
 
 
 @pytest.fixture
-def distance():
-    return [i for i in range(10)]
+def distances():
+    return [[0, 1, 2, 3, 4, 5, 6], [6, 7, 8, 9]]
 
 
 @pytest.fixture
 def x_tics():
-    labels = ["A", "B", "C|D", "E"]
+    labels = ["A", "B", "C$\\mid$D", "E"]
     distances = [0.0, 5.0, 6.0, 9.0]
-    return XTics(labels, distances)
+    return XTicks(labels, distances)
 
 
 @pytest.fixture
@@ -54,101 +63,128 @@ def y_range():
 
 
 @pytest.fixture
-def band_set():
-    band_energies = {Spin.up: [[-3.0, -2, -1,  0, -1, -2, -3, -4, -5, -6],
-                               [ 7.0,  6,  5,  4,  3,  2,  3,  2,  3,  4]]}
+def band_info_set():
+    # band_energies = [{Spin.up: [[-3.0, -2, -1,  0, -1, -2, -3, -4, -5, -6],
+    #                            [ 7.0,  6,  5,  4,  3,  2,  3,  2,  3,  4]]}]
+    band_energies_by_branch = \
+        [{Spin.up: [[-3.0, -2, -1,  0, -1, -2, -3],
+                    [ 7.0,  6,  5,  4,  3,  2,  3]]},
+         {Spin.up: [[-4, -5, -6, -7],
+                    [ 5,  2,  7,  8]]}]
     band_edge = BandEdge(vbm=0, cbm=2, vbm_distances=[3], cbm_distances=[5, 7])
     fermi_level = 1.5
-    return [BandInfo(band_energies=band_energies,
+    return [BandInfo(band_energies_by_branch=band_energies_by_branch,
                      band_edge=band_edge,
                      fermi_level=fermi_level)]
 
 
 @pytest.fixture
-def mock_plt(band_set, distance, x_tics, y_range, mocker):
-    mock_plt = mocker.patch("vise.analyzer.plot_band.plt", auto_spec=True)
-    plotter = BandPlotter(band_set, distance, x_tics, y_range)
-    plotter.construct_plot()
-    return mock_plt
-
-
-def test_set_band_structures(band_set, distance, mock_plt):
-    mock_plt.plot.assert_any_call(distance,
-                                  band_set[0].band_energies[Spin.up][0],
-                                  **band_set[0].band_structure_args,
-                                  )
-    mock_plt.plot.assert_any_call(distance,
-                                  band_set[0].band_energies[Spin.up][1],
-                                  **band_set[0].band_structure_args,
-                                  )
-
-
-def test_set_band_edge_circles(band_set, mock_plt):
-    mock_plt.scatter.assert_any_call(band_set[0].band_edge.vbm_distances[0],
-                                     band_set[0].band_edge.vbm)
-    mock_plt.scatter.assert_any_call(band_set[0].band_edge.cbm_distances[0],
-                                     band_set[0].band_edge.cbm)
-    mock_plt.scatter.assert_any_call(band_set[0].band_edge.cbm_distances[1],
-                                     band_set[0].band_edge.cbm)
-    mock_plt.axhline.assert_any_call(y=band_set[0].band_edge.vbm)
-    mock_plt.axhline.assert_any_call(y=band_set[0].band_edge.cbm)
-
-
-def test_set_fermi_level(band_set, mock_plt):
-    mock_plt.axhline.assert_called_with(y=band_set[0].fermi_level)
-
-
-def test_set_x_range(distance, mock_plt):
-    mock_plt.xlim.assert_called_with(distance[0], distance[-1])
-
-
-def test_set_y_range(y_range, mock_plt):
-    mock_plt.ylim.assert_called_with(y_range[0], y_range[1])
-
-
-def test_set_axes(mock_plt):
-    mock_plt.xlabel.assert_called_with("Wave vector")
-    mock_plt.ylabel.assert_called_with("Energy (eV)")
-
-
-@pytest.fixture
-def mocker_axis(band_set, distance, x_tics, y_range, mocker):
+def mock_plt_axis(band_info_set, distances, x_tics, y_range, mocker):
     mock_plt = mocker.patch("vise.analyzer.plot_band.plt", auto_spec=True)
     mock_axis = MagicMock()
     mock_plt.gca.return_value = mock_axis
-    plotter = BandPlotter(band_set, distance, x_tics, y_range)
+    plotter = BandPlotter(band_info_set, distances, x_tics, y_range)
     plotter.construct_plot()
     return mock_plt, mock_axis
 
 
-def test_set_x_tics(x_tics, mocker_axis):
-    mock_plt, mock_axis = mocker_axis
+def test_defaults_class():
+    band_defaults = BandPlotterDefaults()
+
+    assert band_defaults.band_linestyle == ["-"]
+    assert band_defaults.band_colors == ["red", "blue"]
+    assert band_defaults.band_linewidth == 1.0
+    assert band_defaults.band_edge_circle_size == 100
+
+    band_defaults = BandPlotterDefaults(band_linestyle=["--"],
+                                        band_colors=["black"],
+                                        band_linewidth=2.0,
+                                        band_edge_circle_size=200)
+
+    assert band_defaults.band_linestyle == ["--"]
+    assert band_defaults.band_colors == ["black"]
+    assert band_defaults.band_linewidth == 2.0
+    assert band_defaults.band_edge_circle_size == 200
+
+
+def test_set_band_structures(band_info_set, distances, mock_plt_axis):
+    mock_plt, _ = mock_plt_axis
+    band_defaults = BandPlotterDefaults()
+    mock_plt.plot.assert_any_call(distances[0],
+                                  band_info_set[0].band_energies_by_branch[0][Spin.up][0],
+                                  **band_defaults.band_args)
+    mock_plt.plot.assert_any_call(distances[1],
+                                  band_info_set[0].band_energies_by_branch[1][Spin.up][0],
+                                  **band_defaults.band_args)
+    mock_plt.plot.assert_any_call(distances[0],
+                                  band_info_set[0].band_energies_by_branch[0][Spin.up][1],
+                                  **band_defaults.band_args)
+    mock_plt.plot.assert_any_call(distances[1],
+                                  band_info_set[0].band_energies_by_branch[1][Spin.up][1],
+                                  **band_defaults.band_args)
+
+
+def test_set_band_edge_circles(band_info_set, mock_plt_axis):
+    mock_plt, _ = mock_plt_axis
+    band_edge = band_info_set[0].band_edge
+    band_defaults = BandPlotterDefaults()
+
+    mock_plt.scatter.assert_any_call(band_edge.vbm_distances[0], band_edge.vbm,
+                                     **band_defaults.band_edge_circles_args)
+    mock_plt.scatter.assert_any_call(band_edge.cbm_distances[0], band_edge.cbm,
+                                     **band_defaults.band_edge_circles_args)
+    mock_plt.scatter.assert_any_call(band_edge.cbm_distances[1], band_edge.cbm,
+                                     **band_defaults.band_edge_circles_args)
+    mock_plt.axhline.assert_any_call(y=band_edge.vbm)
+    mock_plt.axhline.assert_any_call(y=band_edge.cbm)
+
+
+def test_set_fermi_level(band_info_set, mock_plt_axis):
+    mock_plt, _ = mock_plt_axis
+    mock_plt.axhline.assert_called_with(y=band_info_set[0].fermi_level)
+
+
+def test_set_x_range(distances, mock_plt_axis):
+    mock_plt, _ = mock_plt_axis
+    mock_plt.xlim.assert_called_with(distances[0][0], distances[-1][-1])
+
+
+def test_set_y_range(y_range, mock_plt_axis):
+    mock_plt, _ = mock_plt_axis
+    mock_plt.ylim.assert_called_with(y_range[0], y_range[1])
+
+
+def test_set_axes(mock_plt_axis):
+    mock_plt, _ = mock_plt_axis
+    mock_plt.xlabel.assert_called_with("Wave vector")
+    mock_plt.ylabel.assert_called_with("Energy (eV)")
+
+
+def test_set_x_tics(x_tics, mock_plt_axis):
+    mock_plt, mock_axis = mock_plt_axis
 
     mock_axis.set_xticks.assert_called_once_with(x_tics.distances)
     mock_axis.set_xticklabels.assert_called_once_with(x_tics.labels)
-    mock_plt.axvline.assert_any_call(x=x_tics.distances[2])
+    mock_plt.axvline.assert_any_call(x=x_tics.distances[2], linestyle="-")
     mock_plt.axvline.assert_any_call(x=x_tics.distances[1], linestyle="--")
 
 
-def test_set_float_to_int_formatter(mocker_axis):
-    mock_plt, mock_axis = mocker_axis
-    mock_axis.xaxis.set_major_formatter.assert_called_once_with(
-        float_to_int_formatter)
+def test_set_float_to_int_formatter(mock_plt_axis):
+    _, mock_axis = mock_plt_axis
     mock_axis.yaxis.set_major_formatter.assert_called_once_with(
         float_to_int_formatter)
 
 
-def test_plot_tight_layout(mock_plt):
+def test_plot_tight_layout(mock_plt_axis):
+    mock_plt, _ = mock_plt_axis
     mock_plt.tight_layout.assert_called_once_with()
 
 
-def test_draw_bands(band_set, distance, x_tics, y_range):
-    band_plotter = BandPlotter(band_set, distance, x_tics, y_range)
+#@pytest.mark.skip()
+def test_draw_bands(band_info_set, distances, x_tics, y_range):
+    band_plotter = BandPlotter(band_info_set, distances, x_tics, y_range)
     band_plotter.construct_plot()
     band_plotter.plt.show()
-
-
-
 
 
 def test_greek_to_unicode():
@@ -158,36 +194,48 @@ def test_greek_to_unicode():
 
 
 def test_italic_to_roman():
-    assert italic_to_roman("A_0") == "${\\rm A}_0$"
+    assert italic_to_roman(r"S$\mid$$S_0$") == "S$\mid$${\\rm S}_0$"
 
 
+test_data = [(True,  None),
+             (False, BandEdge(vbm=-100, cbm=100,
+                              vbm_distances=[0], cbm_distances=[1, 2]))]
 
 
+@pytest.mark.parametrize("is_metal,expected", test_data)
+def test_vasp_band_plotter(is_metal, expected, mocker):
+    stub_vasprun = mocker.MagicMock(spec=Vasprun)
+    mock_bs = mocker.MagicMock()
+    mock_bs.efermi = 10
+    mock_bs.is_metal.return_value = is_metal
+
+    stub_vasprun.get_band_structure.return_value = mock_bs
+
+    mock_bsp = mocker.patch("vise.analyzer.plot_band.BSPlotter", auto_spec=True)
+    energy = [{Spin.up: [[0.1]]}]
+    distances = [[0.0, 0.1, 0.2]]
+    labels = ["A", "A_0", "GAMMA"]
+    label_distances = [0.0, 0.1, 0.2]
+    plot_data = {"ticks": {"label": labels, "distance": label_distances},
+                 "energy": energy,
+                 "distances": distances,
+                 "vbm": [[0, -100]],
+                 "cbm": [[1, 100], [2, 100]]}
+    mock_bsp().bs_plot_data.return_value = plot_data
+
+    plotter = VaspBandPlotter(stub_vasprun, "KPOINTS")
+    assert plotter.band_info_set == [BandInfo(band_energies_by_branch=energy,
+                                              band_edge=expected,
+                                              fermi_level=10)]
+    assert plotter.distances_by_branch == distances
+    assert plotter.x_ticks == XTicks(labels=["A", "${\\rm A}_0$", "Γ"],
+                                     distances=label_distances)
+    assert plotter.y_range == [-10, 10]
 
 
-def test_pairwise():
-    assert list(pairwise([0, 1, 2, 3])) == [(0, 1), (1, 2), (2, 3)]
-
-
-
-
-# @pytest.fixture
-# def xtics_with_overlap():
-#     labels = ["A", "B", "C", "D", "E", "F"]
-#     distances = [0.0, 5.0, 5.0, 7.0, 7.0, 9.0]
-#     return Xtics(labels, distances)
-
-
-# def test_assert_incorrect_distances():
-#     labels = ["A", "B", "C"]
-#     distances = [0.0, 6.0, 5.0]
-#     with pytest.raises(AssertionError):
-#         Xtics(labels, distances)
-
-
-# def test_xtics_with_overlap(xtics_with_overlap):
-#     assert xtics_with_overlap.labels == ["A", "B|C", "D|E", "F"]
-#     assert xtics_with_overlap.distances == [0.0, 5.0, 7.0, 9.0]
-#     assert xtics_with_overlap.vertical_line_positions == [5.0, 7.0]
-
+def test_vasp_band_plotter_with_actual_files(test_data_files: Path):
+    vasprun = Vasprun(str(test_data_files / "KO2_band_vasprun.xml"))
+    plotter = VaspBandPlotter(vasprun, str(test_data_files / "KO2_band_KPOINTS"))
+    plotter.construct_plot()
+    plotter.plt.show()
 
