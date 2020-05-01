e8de1f65c93601398a7a867c51d0841fd1aac066
yuuukuma <yuuukuma@gmail.com>
2020-05-01 21:59:21 +0900

    Refactor FileTransfers and its unittests

--- vise/tests/util/test_file_transfer.py
+++ vise/tests/util/test_file_transfer.py
@@ -8,8 +8,9 @@ import tempfile
 import pytest
 
 from vise.util.file_transfer import (
-    FileTransfer, FileMove, FileCopy, FileLink, FileTransfers)
-
+    FileTransfer, FileMove, FileCopy, FileLink, FileTransfers,
+    ViseFileTransferError
+)
 
 cwd = Path.cwd().absolute()
 
@@ -35,63 +36,52 @@ def test_file_transfer_to(file_transfer_instance):
     assert file_transfer_instance.to(Path("b")) == cwd / "b" / "a"
 
 
-def test_file_move():
+@pytest.fixture
+def tmp_dirs():
     print_string = "test"
     filename = "a"
     with tempfile.TemporaryDirectory() as tmp_from:
-        file_move = FileMove(abs_file_path=Path(tmp_from) / filename)
         os.chdir(tmp_from)
 
         with open(filename, "w") as f1:
             print(print_string, end="", file=f1)
 
         with tempfile.TemporaryDirectory() as tmp_to:
-            file_move.transfer(Path(tmp_to))
-            os.chdir(Path(tmp_to))
-
-            with open(Path(tmp_to) / filename, 'r') as f2:
-                assert f2.read() == print_string
+            yield tmp_from, tmp_to, filename, print_string
 
 
-def test_file_copy():
-    print_string = "test"
-    filename = "a"
-    with tempfile.TemporaryDirectory() as tmp_from:
-        file_copy = FileCopy(abs_file_path=Path(tmp_from) / filename)
-        os.chdir(tmp_from)
+def test_file_move(tmp_dirs):
+    tmp_from, tmp_to, filename, print_string = tmp_dirs
+    file_move = FileMove(abs_file_path=Path(tmp_from) / filename)
+    file_move.transfer(Path(tmp_to))
+    os.chdir(Path(tmp_to))
 
-        with open(filename, "w") as f1:
-            print(print_string, end="", file=f1)
-
-        with tempfile.TemporaryDirectory() as tmp_to:
-            file_copy.transfer(Path(tmp_to))
-            os.chdir(Path(tmp_to))
-
-            with open(Path(tmp_to) / filename, 'r') as f_to:
-                with open(Path(tmp_from) / filename, 'r') as f_from:
-                    assert f_to.read() == f_from.read()
+    with open(Path(tmp_to) / filename, 'r') as f2:
+        assert f2.read() == print_string
 
 
-def test_file_link():
-    print_string = "test"
-    filename = "a"
-    with tempfile.TemporaryDirectory() as tmp_from:
-        file_link = FileLink(abs_file_path=Path(tmp_from) / filename)
-        os.chdir(tmp_from)
+def test_file_copy(tmp_dirs):
+    tmp_from, tmp_to, filename, _ = tmp_dirs
+    file_copy = FileCopy(abs_file_path=Path(tmp_from) / filename)
+    file_copy.transfer(Path(tmp_to))
+    os.chdir(Path(tmp_to))
 
-        with open(filename, "w") as f1:
-            print(print_string, end="", file=f1)
+    with open(Path(tmp_to) / filename, 'r') as f_to:
+        with open(Path(tmp_from) / filename, 'r') as f_from:
+            assert f_to.read() == f_from.read()
 
-        with tempfile.TemporaryDirectory() as tmp_to:
-            file_link.transfer(Path(tmp_to))
 
-            assert (Path(tmp_to) / filename).is_symlink()
+def test_file_link(tmp_dirs):
+    tmp_from, tmp_to, filename, _ = tmp_dirs
+    file_link = FileLink(abs_file_path=Path(tmp_from) / filename)
+    file_link.transfer(Path(tmp_to))
 
-            os.chdir(Path(tmp_to))
+    assert (Path(tmp_to) / filename).is_symlink()
 
-            with open(Path(tmp_to) / filename, 'r') as f_to:
-                with open(Path(tmp_from) / filename, 'r') as f_from:
-                    assert f_to.read() == f_from.read()
+    os.chdir(Path(tmp_to))
+    with open(Path(tmp_to) / filename, 'r') as f_to:
+        with open(Path(tmp_from) / filename, 'r') as f_from:
+            assert f_to.read() == f_from.read()
 
 
 def test_transfer_files():
@@ -101,16 +91,18 @@ def test_transfer_files():
         for i in ["file1", "file2", "file3", "file4"]:
             with open(i, "w") as f1:
                 print(test_string, end="", file=f1)
-        file_transfers = FileTransfers.from_dict(
+        file_transfers = FileTransfers(
             {"file1": "m", "file2": "c", "file3": "l", "file4": "m"},
             path=Path(tmp_from))
 
         with tempfile.TemporaryDirectory() as tmp_to:
             file_transfers.transfer(Path(tmp_to))
+            # Test copied and linked files
             for i in ["file2", "file3"]:
                 with open(Path(tmp_to) / i, 'r') as f_to:
                     with open(Path(tmp_from) / i, 'r') as f_from:
                         assert f_to.read() == f_from.read()
+            # Test moved files
             for i in ["file1", "file4"]:
                 with open(Path(tmp_to) / i, 'r') as f2:
                     assert f2.read() == test_string
@@ -122,7 +114,7 @@ def test_transfer_files_logger_non_exist(mocker):
 
     with tempfile.TemporaryDirectory() as tmp_from:
         os.chdir(tmp_from)
-        FileTransfers.from_dict({non_exist_filename: "m"}, path=Path(tmp_from))
+        FileTransfers({non_exist_filename: "m"}, path=Path(tmp_from))
         mock.assert_called_once_with(
             f"{Path(tmp_from) / non_exist_filename} does not exist.")
 
@@ -135,23 +127,18 @@ def test_transfer_files_logger_empty(mocker):
         os.chdir(tmp_from)
         with open(empty_filename, "w") as f1:
             print("", end="", file=f1)
-        FileTransfers.from_dict({empty_filename: "m"}, path=Path(tmp_from))
+        FileTransfers({empty_filename: "m"}, path=Path(tmp_from))
         mock.assert_called_once_with(
             f"{Path(tmp_from) / empty_filename} is empty.")
 
 
-def test_transfer_files_logger_warning(mocker):
-    mock = mocker.patch("vise.util.file_transfer.logger.warning")
-    filename = "c"
-    print_string = "test"
-
-    with tempfile.TemporaryDirectory() as tmp_from:
-        os.chdir(tmp_from)
-        with open(filename, "w") as f2:
-            print(print_string, end="", file=f2)
-        FileTransfers.from_dict({filename: "x"}, path=Path(tmp_from))
-        mock.assert_called_once_with(
-            f"x option for {filename} is invalid.")
+def test_raise_error_for_incorrect_transfer_type():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        os.chdir(tmp_dir)
+        with open("filename", "w") as f1:
+            print("a", end="", file=f1)
+        with pytest.raises(ViseFileTransferError):
+            FileTransfers({"filename": "x"}, path=Path(tmp_dir))
 
 
 def test_transfer_delete():
@@ -160,11 +147,11 @@ def test_transfer_delete():
         for i in ["a", "bbb", "cbb", "bd", "bda"]:
             with open(i, "w") as f1:
                 print("test", end="", file=f1)
-        file_transfers = FileTransfers.from_dict({"a":   "m",
-                                                  "bbb": "c",
-                                                  "cbb": "l",
-                                                  "bd":  "m",
-                                                  "bda": "m"},
-                                                 path=Path(tmp_from))
+        file_transfers = FileTransfers({"a":   "m",
+                                        "bbb": "c",
+                                        "cbb": "l",
+                                        "bd":  "m",
+                                        "bda": "m"},
+                                       path=Path(tmp_from))
         file_transfers.delete_file_transfers_w_keywords(["bb", "a"])
-        assert len(file_transfers.file_transfer_list) == 1
+        assert len(file_transfers.file_transfers) == 1
--- vise/util/file_transfer.py
+++ vise/util/file_transfer.py
@@ -48,44 +48,45 @@ class FileLink(FileTransfer):
         os.symlink(self.abs_in_file, self.to(abs_output_dir))
 
 
-class FileTransfers:
-    def __init__(self, file_transfer_list: List[FileTransfer]):
-        self.file_transfer_list = file_transfer_list
+def transfer_instance(initial, filename):
+    if initial == "m":
+        result = FileMove
+    elif initial == "c":
+        result = FileCopy
+    elif initial == "l":
+        result = FileLink
+    else:
+        raise ViseFileTransferError
+    return result(filename)
 
-    @classmethod
-    def from_dict(cls,
-                  transfer_manner_by_file: Dict[str, str],
-                  path: Path) -> "FileTransfers":
+
+class FileTransfers:
+    def __init__(self, transfer_manner_by_file: Dict[str, str], path: Path):
 
         file_transfers = []
         for filename, transfer_manner in transfer_manner_by_file.items():
             f = path.absolute() / filename
             initial = transfer_manner[0].lower()
+
             if not f.is_file():
                 logger.warning(f"{f} does not exist.")
             elif f.stat().st_size == 0:
                 logger.warning(f"{f} is empty.")
             else:
-                if initial == "m":
-                    transfer_cls = FileMove
-                elif initial == "c":
-                    transfer_cls = FileCopy
-                elif initial == "l":
-                    transfer_cls = FileLink
-                else:
-                    logger.warning(
-                        f"{transfer_manner} option for {filename} is invalid.")
-                    continue
-                file_transfers.append(transfer_cls(f))
+                file_transfers.append(transfer_instance(initial, f))
 
-        return cls(file_transfers)
+        self.file_transfers = file_transfers
 
     def delete_file_transfers_w_keywords(self, keywords: List[str]):
         pattern = re.compile("|".join(keywords))
-        for a_file_transfer in list(self.file_transfer_list):
+        for a_file_transfer in list(self.file_transfers):
             if pattern.search(a_file_transfer.file_name):
-                self.file_transfer_list.remove(a_file_transfer)
+                self.file_transfers.remove(a_file_transfer)
 
     def transfer(self, output_dir: Path) -> None:
-        for transfer in self.file_transfer_list:
+        for transfer in self.file_transfers:
             transfer.transfer(output_dir.absolute())
+
+
+class ViseFileTransferError(Exception):
+    pass
