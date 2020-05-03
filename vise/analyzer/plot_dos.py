5eeeb630215770875b0464abafa8b238939fdae0
yuuukuma <yuuukuma@gmail.com>
2020-05-03 20:02:42 +0900

    Update plot_dos.py and its unittests

--- vise/analyzer/plot_dos.py
+++ vise/analyzer/plot_dos.py
@@ -1,32 +1,106 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 
-from typing import List, Dict
-import numpy as np
-import matplotlib.pyplot as plt
+from dataclasses import dataclass
+from typing import List, Dict, Optional
 
-from pymatgen import Spin
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+@dataclass
+class OrbitalDos:
+    name: str
+    dos: List[List[float]]
+
+    def max_each_dos(self):
+        return np.max(np.array(self.dos))
+
+
+class DosInfo:
+
+    def __init__(self,
+                 energies: List[float],
+                 doses: List[List[OrbitalDos]],
+                 xlim: Optional[List[float]] = None,
+                 ylim_set: Optional[List[List[float]]] = None):
+        self.energies = energies
+        self.doses = doses
+        self.xlim = xlim or [-10, 10]
+        self.ylim_set = ylim_set or [[-y, y] for y in self.max_y_ranges()]
+
+    def max_y_ranges(self, multi=1.1, round_digit=2):
+        return [round(i * multi, round_digit) for i in self.max_abs_doses()]
+
+    def max_abs_doses(self):
+        result = []
+        for dos in self.doses:
+            result.append(np.max([orb_dos.max_each_dos() for orb_dos in dos]))
+        return result
+
+
+class DosMplDefaults:
+    def __init__(self,
+                 colors: Optional[List[str]] = None,
+                 linewidth: float = 1.0,
+                 band_edge_line_width: float = 0.75,
+                 band_edge_line_color: str = "black",
+                 band_edge_line_style: str = "-.",
+                 title_font_size: int = 15,
+                 label_font_size: int = 15,
+                 ):
+        self.colors = colors or ['#36454f', '#E15759', '#4E79A7', '#F28E2B',
+                                 '#76B7B2']
+        self.linewidth = linewidth
+
+        self.vline = {"linewidth": band_edge_line_width,
+                      "color": band_edge_line_color,
+                      "linestyle": band_edge_line_style}
+
+        self.title_font_size = title_font_size
+        self.label_font_size = label_font_size
+
+    def dos_line(self, index):
+        return {"color": self.colors[index],
+                "linewidth": self.linewidth}
 
 
 class DosPlotter:
     def __init__(self,
-                 energies: List[float],
-                 doses: List[Dict[str, Dict[Spin, List[float]]]]):
-        self.energies = energies
-        self.doses = doses
+                 dos_info: DosInfo,
+                 mpl_defaults: Optional[DosMplDefaults] = DosMplDefaults()):
+        self._dos_info = dos_info
+        self.mpl_defaults = mpl_defaults
+
         self.plt = plt
+        num_axs = len(self._dos_info.doses)
+        _, self._axs = self.plt.subplots(num_axs, 1, sharex=True)
 
     def construct_plot(self):
+        self._axs[0].set_xlim(self._dos_info.xlim)
+        for i in range(len(self._dos_info.doses)):
+            self._add_ax(i)
 
-        _, axs = self.plt.subplots(2, 1, sharex=True)
+    def _add_ax(self, i):
+        self._add_dos(i)
+        self._set_y_range(i)
+        self._set_dos_zero_line(i)
 
-        for i, dos in enumerate(self.doses):
-            ax = axs[i]
-            for name, dos_each_name in dos.items():
-                for spin, dos_each_spin in dos_each_name.items():
-                    dos_for_plot = [d * int(spin) for d in dos_each_spin]
-                    ax.plot(self.energies, dos_for_plot)
-#            self.plt.legend()
+    def _add_dos(self, i):
+        for j, dos_each_name in enumerate(self._dos_info.doses[i]):
+            for k, dos_each_spin in enumerate(dos_each_name.dos):
+                sign = - (2 * k) + 1
+                dos_for_plot = [d * sign for d in dos_each_spin]
+                args = self.mpl_defaults.dos_line(j)
+                if k == 0:
+                    args["label"] = self._dos_info.doses[i][j].name
+                self._axs[i].plot(self._dos_info.energies, dos_for_plot, **args)
 
-            ax.axhline(0)
+        self._axs[i].legend(loc="best", markerscale=0.1)
+
+    def _set_dos_zero_line(self, i):
+        self._axs[i].axhline(0, linestyle=":", color="black")
+
+    def _set_y_range(self, i):
+        self._axs[i].set_ylim(self._dos_info.ylim_set[i])
 
--- vise/tests/analyzer/test_plot_dos.py
+++ vise/tests/analyzer/test_plot_dos.py
@@ -6,20 +6,14 @@ from collections import OrderedDict
 
 from pymatgen import Spin
 
-from vise.analyzer.plot_dos import DosPlotter
+from vise.analyzer.plot_dos import OrbitalDos, DosInfo, DosPlotter, DosMplDefaults
 
 """
 TODO:
-+ Use +- same scale for total with the ylim max is 1.1 * max of dos
-+ Use +- same scale for pdos with the ylim max is 1.1 * max of dos
-+ Use consistent scale for pdos.
++ Add labels
 
-+ Allow to set x-scale  
-+ Allow to set y-scale for total
-+ Allow to set y-scale for pdos
 + Allow to set vbm, cbm, efermi
 + Shift energy zero to vbm or efermi.
-+ Add labels
 
 
 DONE:
@@ -27,6 +21,9 @@ DONE:
 + Show spin down as well.
 + Add zero at y=0.
 + Show two data.
++ Allow to set y-scale for total
++ Allow to set y-scale for pdos
++ Allow to set x-scale  
 """
 
 
@@ -37,82 +34,169 @@ def energies():
 
 @pytest.fixture
 def total_up():
-    return [0] * 2 + [4] * 8 + [0]
+    return [0.0] * 2 + [2] * 2 + [4] * 4 + [2] * 2 + [0]
 
 
 @pytest.fixture
 def total_down():
-    return [0] * 4 + [6] * 6 + [0]
+    return [0.0] * 4 + [6] * 6 + [0]
 
 
 @pytest.fixture
 def h_s_up():
-    return [0] * 2 + [2] * 8 + [0]
+    return [0.0] * 2 + [2] * 8 + [0]
 
 
 @pytest.fixture
 def h_s_down():
-    return [0] * 4 + [1] * 6 + [0]
+    return [0.0] * 4 + [1] * 6 + [0]
 
 
 @pytest.fixture
 def h_p_up():
-    return [0] * 2 + [3] * 8 + [0]
+    return [0.0] * 2 + [3] * 8 + [0]
 
 
 @pytest.fixture
 def h_p_down():
-    return [0] * 4 + [1] * 6 + [0]
+    return [0.0] * 3 + [2] * 7 + [0]
 
 
 @pytest.fixture
 def doses(total_up, total_down, h_s_up, h_s_down, h_p_up, h_p_down):
 
-    total_dos = {Spin.up: total_up, Spin.down: total_down}
+    total_dos = [OrbitalDos("total", [total_up, total_down])]
 
-    h_s = {Spin.up: h_s_up, Spin.down: h_s_down}
-    h_p = {Spin.up: h_p_up, Spin.down: h_p_down}
+    h_s = OrbitalDos("H-s", [h_s_up, h_s_down])
+    h_p = OrbitalDos("H-p", [h_p_up, h_p_down])
+    h_dos = [h_s, h_p]
 
-    total_dos_dict = OrderedDict({"total": total_dos})
-    h_dos_dict = OrderedDict({"H-s": h_s, "H-p": h_p})
-
-    return [total_dos_dict, h_dos_dict]
+    return [total_dos, h_dos]
 
 
 @pytest.fixture
-def mock_plt_1st_ax(energies, doses, mocker):
+def dos_info(doses, energies):
+    return DosInfo(energies=energies, doses=doses)
+
+
+@pytest.fixture
+def xlim():
+    return [-6, 6]
+
+
+@pytest.fixture
+def ylim_set():
+    return [[-10, 10], [-5, 5]]
+
+
+@pytest.fixture
+def dos_info_manual_axis(doses, energies, xlim, ylim_set):
+    return DosInfo(energies=energies, doses=doses, xlim=xlim, ylim_set=ylim_set)
+
+
+def test_orbital_dos(total_up, total_down):
+    orbital_dos = OrbitalDos("total", [total_up, total_down])
+    assert orbital_dos.max_each_dos() == 6.0
+
+
+def test_doses(dos_info, energies, total_up, total_down):
+    assert dos_info.energies == energies
+    assert dos_info.doses[0][0].name == "total"
+    assert dos_info.doses[0][0].dos == [total_up, total_down]
+    assert dos_info.xlim == [-10, 10]
+    assert dos_info.ylim_set == [[-6.6, 6.6], [-3.3, 3.3]]
+
+
+def test_doses_manual_axis(dos_info_manual_axis, xlim, ylim_set):
+    assert dos_info_manual_axis.xlim == xlim
+    assert dos_info_manual_axis.ylim_set == ylim_set
+
+
+def colors():
+    return DosMplDefaults().colors
+
+
+def test_dos_mpl_defaults():
+    dos_defaults = DosMplDefaults()
+    assert dos_defaults.colors == ['#36454f', '#E15759', '#4E79A7', '#F28E2B',
+                                   '#76B7B2']
+    assert dos_defaults.linewidth == 1.0
+    assert dos_defaults.title_font_size == 15
+    assert dos_defaults.label_font_size == 15
+    assert dos_defaults.vline == \
+           {"linewidth": 0.75, "color": "black", "linestyle": "-."}
+
+    dos_defaults = DosMplDefaults(colors=["black"],
+                                  linewidth=2.0,
+                                  band_edge_line_width=1.0,
+                                  band_edge_line_color="blue",
+                                  band_edge_line_style=":",
+                                  title_font_size=30,
+                                  label_font_size=40)
+
+    assert dos_defaults.colors == ["black"]
+    assert dos_defaults.linewidth == 2.0
+    assert dos_defaults.title_font_size == 30
+    assert dos_defaults.label_font_size == 40
+    assert dos_defaults.vline == \
+           {"linewidth": 1.0, "color": "blue", "linestyle": ":"}
+
+
+@pytest.fixture
+def mock_plt_1st_ax(dos_info_manual_axis, mocker):
     mock_plt = mocker.patch("vise.analyzer.plot_dos.plt", auto_spec=True)
     mock_1st_ax = mocker.MagicMock()
     mock_2nd_ax = mocker.MagicMock()
-    other_mocks = [mocker.MagicMock()] * (len(doses) - 2)
-    mock_axs = [mock_1st_ax, mock_2nd_ax] + other_mocks
+    other_axs_len = (len(dos_info_manual_axis.doses) - 2)
+
+    mock_axs = [mock_1st_ax, mock_2nd_ax] + [mocker.MagicMock()] * other_axs_len
 
     mock_plt.subplots.return_value = (None, mock_axs)
-    plotter = DosPlotter(energies, doses)
+    plotter = DosPlotter(dos_info=dos_info_manual_axis)
     plotter.construct_plot()
 
     return mock_plt, mock_1st_ax, mock_2nd_ax
 
 
-def test_plot_dos(energies, total_up, total_down, doses, mock_plt_1st_ax):
-    num_axs = len(doses)
-    mock_plt, mock_1st_ax, _ = mock_plt_1st_ax
-
+def test_plot_dos(energies, total_up, total_down, h_s_up, h_p_down, dos_info,
+                  mock_plt_1st_ax):
+    num_axs = len(dos_info.doses)
+    mock_plt, mock_1st_ax, mock_2nd_ax = mock_plt_1st_ax
     mock_plt.subplots.assert_called_once_with(num_axs, 1, sharex=True)
 
-    total_dos_sign_reversed = [dos * -1 for dos in total_down]
+    reversed_total_down = [dos * -1 for dos in total_down]
+    reversed_h_p_down = [dos * -1 for dos in h_p_down]
 
-    mock_1st_ax.plot.assert_any_call(energies, total_up)
-    mock_1st_ax.plot.assert_any_call(energies, total_dos_sign_reversed)
-    mock_1st_ax.axhline.assert_called_once_with(0)
+    args = DosMplDefaults().dos_line(0)
+    args["label"] = "total"
+    mock_1st_ax.plot.assert_any_call(energies, total_up, **args)
+
+    args = DosMplDefaults().dos_line(0)
+    mock_1st_ax.plot.assert_any_call(energies, reversed_total_down, **args)
+
+    mock_1st_ax.axhline.assert_called_once_with(0, linestyle=":", color="black")
+
+    args = DosMplDefaults().dos_line(0)
+    args["label"] = "H-s"
+    mock_2nd_ax.plot.assert_any_call(energies, h_s_up, **args)
+
+    args = DosMplDefaults().dos_line(1)
+    mock_2nd_ax.plot.assert_any_call(energies, reversed_h_p_down, **args)
 
 
-def test_set_y_range(distances, mock_plt_axis):
+def test_set_figure_legend(mock_plt_1st_ax):
+    _, mock_1st_ax, mock_2nd_ax = mock_plt_1st_ax
+    mock_1st_ax.legend.assert_called_with(loc="best", markerscale=0.1)
+    mock_2nd_ax.legend.assert_called_with(loc="best", markerscale=0.1)
+
+
+def test_set_x_and_y_range(mock_plt_1st_ax):
     _, mock_1st_ax, _ = mock_plt_1st_ax
-    mock_1st_ax.xlim.assert_called_with(-6.6, 6.6)
+    mock_1st_ax.set_ylim.assert_called_with([-10, 10])
+    mock_1st_ax.set_xlim.assert_called_with([-6, 6])
 
 
-def test_actual_plot(energies, doses):
-    plotter = DosPlotter(energies, doses)
+def test_actual_plot(dos_info_manual_axis):
+    plotter = DosPlotter(dos_info_manual_axis)
     plotter.construct_plot()
     plotter.plt.show()
