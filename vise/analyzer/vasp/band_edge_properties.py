0ca985422ab9b9b817412345a7eaa8866237740f
yuuukuma <yuuukuma@gmail.com>
2020-05-09 20:55:06 +0900

    Fix bug related to magnetization in vasp band_edge_properties.py

--- vise/analyzer/vasp/band_edge_properties.py
+++ vise/analyzer/vasp/band_edge_properties.py
@@ -13,9 +13,11 @@ class VaspBandEdgeProperties(BandEdgeProperties):
                  outcar: Outcar,
                  integer_criterion: float = 0.1):
 
+        total_mag = outcar.total_mag if outcar.total_mag else 0.0
+
         super().__init__(eigenvalues=eigenvalues_from_vasprun(vasprun),
                          nelect=outcar.nelect,
-                         magnetization=outcar.total_mag,
+                         magnetization=total_mag,
                          kpoints=vasprun.actual_kpoints,
                          integer_criterion=integer_criterion)
 
--- vise/tests/analyzer/vasp/test_band_edge_properties.py
+++ vise/tests/analyzer/vasp/test_band_edge_properties.py
@@ -10,12 +10,6 @@ from vise.analyzer.vasp.band_edge_properties import eigenvalues_from_vasprun, \
 from vise.tests.analyzer.test_band_edge_properties import parent_dir
 
 
-def test_eigenvalues_from_vasprun(test_data_files):
-    vasprun_file = str(test_data_files / "MnO_uniform_vasprun.xml")
-    vasprun = Vasprun(vasprun_file)
-    assert eigenvalues_from_vasprun(vasprun)[Spin.up][1, 2] == -0.1397
-
-
 def test_band_edge_properties_from_vasp(test_data_files):
     vasprun_file = str(test_data_files / "MnO_uniform_vasprun.xml")
     vasprun = Vasprun(vasprun_file)
@@ -24,3 +18,21 @@ def test_band_edge_properties_from_vasp(test_data_files):
     band_edge = VaspBandEdgeProperties(vasprun, outcar)
 
     assert pytest.approx(band_edge.band_gap) == 0.4702
+
+
+def test_band_edge_properties_from_vasp_non_mag(test_data_files):
+    vasprun_file = str(test_data_files / "MgO_band_vasprun.xml")
+    vasprun = Vasprun(vasprun_file)
+    outcar_file = str(test_data_files / "MgO_band_OUTCAR")
+    outcar = Outcar(outcar_file)
+    band_edge = VaspBandEdgeProperties(vasprun, outcar)
+
+    assert pytest.approx(band_edge.band_gap) == 4.6597
+
+
+def test_eigenvalues_from_vasprun(test_data_files):
+    vasprun_file = str(test_data_files / "MnO_uniform_vasprun.xml")
+    vasprun = Vasprun(vasprun_file)
+    assert eigenvalues_from_vasprun(vasprun)[Spin.up][1, 2] == -0.1397
+
+
