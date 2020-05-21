34addd3531590e655816c124eda9fae03255e6ff
yuuukuma <yuuukuma@gmail.com>
2020-05-21 20:18:44 +0900

    Add assert_msonable to conftest.py

--- vise/tests/conftest.py
+++ vise/tests/conftest.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import json
 from pathlib import Path
 
 import pytest
+from monty.json import MSONable, MontyDecoder
 from pymatgen import Lattice
 from pymatgen.core.structure import Structure
 
@@ -38,3 +39,8 @@ def complex_monoclinic_structure():
                      species=["H", "He", "He", "He", "He"],
                      coords=coords)
 
+
+def assert_msonable(obj):
+    assert isinstance(obj, MSONable)
+    assert obj.as_dict() == obj.__class__.from_dict(obj.as_dict()).as_dict()
+    assert json.loads(obj.to_json(), cls=MontyDecoder)
