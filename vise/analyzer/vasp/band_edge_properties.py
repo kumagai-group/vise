d436b2f13f4291be86a584f056c8d09cac343eea
yuuukuma <yuuukuma@gmail.com>
2020-07-02 16:00:28 +0900

    Add edge_orbital_contributions in band_edge_properties.py

--- vise/analyzer/vasp/band_edge_properties.py
+++ vise/analyzer/vasp/band_edge_properties.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
+from collections import defaultdict
 
-from pymatgen.io.vasp import Vasprun, Outcar
+import numpy
+from pymatgen.io.vasp import Vasprun, Outcar, Procar
 
 from vise.analyzer.band_edge_properties import BandEdgeProperties
 
@@ -15,11 +17,49 @@ class VaspBandEdgeProperties(BandEdgeProperties):
 
         total_mag = outcar.total_mag if outcar.total_mag else 0.0
 
+        self.structure = vasprun.final_structure
+
         super().__init__(eigenvalues=eigenvalues_from_vasprun(vasprun),
                          nelect=outcar.nelect,
                          magnetization=total_mag,
                          kpoints=vasprun.actual_kpoints,
                          integer_criterion=integer_criterion)
+
+
+def edge_orbital_contributions(procar: Procar, structure, vbm_info, cbm_info):
+    elements = structure.composition.elements
+    ele_indices = []
+    for e in elements:
+        ele_indices.append(
+            [i for i, s in enumerate(structure) if s.specie == e])
+
+    # Kpoint index first and then band index
+    vbm = procar.data[vbm_info.spin][vbm_info.kpoint_index][vbm_info.band_index]
+    cbm = procar.data[cbm_info.spin][cbm_info.kpoint_index][cbm_info.band_index]
+
+    # orbital type
+    if len(vbm[0]) == 3:
+        orb_indices = [[0], [1], [2]]
+    elif len(vbm[0]) == 4:
+        orb_indices = [[0], [1], [2], [3]]
+    elif len(vbm[0]) == 9:
+        orb_indices = [[0], [1, 2, 3], [4, 5, 6, 7, 8]]
+    elif len(vbm[0]) == 16:
+        orb_indices = [[0], [1, 2, 3], [4, 5, 6, 7, 8],
+                       [9, 10, 11, 12, 13, 14, 15]]
+    else:
+        raise ValueError("")
+
+    vbm_comp, cbm_comp = defaultdict(dict), defaultdict(dict)
+
+    print(vbm)
+
+    for e, e_idxs in zip(elements, ele_indices):
+        for o, o_idxs in zip(["s", "p", "d", "f"], orb_indices):
+            vbm_comp[str(e)][o] = vbm[numpy.ix_(e_idxs, o_idxs)].sum()
+            cbm_comp[str(e)][o] = cbm[numpy.ix_(e_idxs, o_idxs)].sum()
+
+    return dict(vbm_comp), dict(cbm_comp)
 
 
 def eigenvalues_from_vasprun(vasprun):
--- vise/tests/analyzer/vasp/test_band_edge_properties.py
+++ vise/tests/analyzer/vasp/test_band_edge_properties.py
@@ -2,11 +2,12 @@
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 
 import pytest
-from pymatgen import Spin
-from pymatgen.io.vasp import Vasprun, Outcar
+from pymatgen import Spin, Structure, Lattice
+from pymatgen.io.vasp import Vasprun, Outcar, Procar
+from vise.analyzer.band_edge_properties import BandEdge
 
 from vise.analyzer.vasp.band_edge_properties import eigenvalues_from_vasprun, \
-    VaspBandEdgeProperties
+    VaspBandEdgeProperties, edge_orbital_contributions
 from vise.tests.analyzer.test_band_edge_properties import parent_dir
 
 
@@ -16,7 +17,6 @@ def test_band_edge_properties_from_vasp(test_data_files):
     outcar_file = str(test_data_files / "MnO_uniform_OUTCAR")
     outcar = Outcar(outcar_file)
     band_edge = VaspBandEdgeProperties(vasprun, outcar)
-
     assert pytest.approx(band_edge.band_gap) == 0.4702
 
 
@@ -28,6 +28,21 @@ def test_band_edge_properties_from_vasp_non_mag(test_data_files):
     band_edge = VaspBandEdgeProperties(vasprun, outcar)
 
     assert pytest.approx(band_edge.band_gap) == 4.6597
+
+
+def test_band_edge_properties_orbital_contributions(test_data_files):
+    procar = Procar(test_data_files / "MgO_band_PROCAR")
+    structure = Structure(
+        Lattice.cubic(1), species=["Mg", "O"], coords=[[0]*3, [0.5]*3])
+    vbm_info = BandEdge.from_dict({'energy': 3.0663, 'spin': 1, 'band_index': 3,
+                                   'kpoint_index': 0,
+                                   'kpoint_coords': [0.0, 0.0, 0.0]})
+    cbm_info = BandEdge.from_dict({'energy': 7.726, 'spin': 1, 'band_index': 4,
+                                   'kpoint_index': 0,
+                                   'kpoint_coords': [0.0, 0.0, 0.0]})
+    vbm, cbm = edge_orbital_contributions(procar, structure, vbm_info, cbm_info)
+    assert vbm["O"]["p"] == 0.753
+    assert cbm["Mg"]["s"] == 0.238
 
 
 def test_eigenvalues_from_vasprun(test_data_files):
