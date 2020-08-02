1cd6467607983be712fa0a0ef87ca40db1e410d6
yuuukuma <yuuukuma@gmail.com>
2020-08-02 11:59:55 +0900

    Add Zn fake POTCAR and create_fake_potcar.sh

---
+++ vise/tests/test_data_files/fake_potcars/POT_GGA_PAW_PBE_54/create_fake_potcar.sh
@@ -0,0 +1,7 @@
+head -n 5 POTCAR                                                                      
+grep TITEL POTCAR
+grep POMASS POTCAR
+grep ENMAX POTCAR
+echo """
+   END of PSCTR-controll parameters
+ End of Dataset"""
