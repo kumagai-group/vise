782ede98f50e9b827ed513fb3c00bb8bfc09b443
yuuukuma <yuuukuma@gmail.com>
2020-08-08 14:42:59 +0900

    Add make_atom_vasp_set.py

---
+++ vise/tests/input_set/test_make_atom_vasp_set.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+import os
+
+import pytest
+from vise.input_set.datasets.make_atom_vasp_set import make_atom_vasp_set
+from vise.input_set.datasets.potcar_set import PotcarSet
+from vise.input_set.xc import Xc
+
+#
+# def test(tmpdir, mocker):
+#     tmpdir.chdir()
+#     a = PotcarSet.normal
+#     make_atom_vasp_set(a, Xc.pbesol)
+#
+
+
+"""
+TODO
+- Make H dir with potcar inside
+
+DONE
+"""
