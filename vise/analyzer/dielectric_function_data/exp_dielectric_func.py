c4b58d8e81723612eb1d7e90d1384eaf28be8766
yuuukuma <yuuukuma@gmail.com>
2020-11-07 17:56:35 +0900

    Add exp_dielectric_func.py and GaAs.csv and its test

---
+++ vise/analyzer/dielectric_function_data/exp_dielectric_func.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+from pathlib import Path
+from pandas import DataFrame
+import pandas as pd
+from vise.util.logger import get_logger
+
+logger = get_logger(__name__)
+
+parent_dir = Path(__file__).parent
+
+
+band_gap = {"GaAs": 1.41}
+
+
+class ExpDieleFunc:
+    def __init__(self, material):
+        self.material = material
+        try:
+            self.dataframe = pd.read_csv(parent_dir / f"{material}.csv", sep='\s+')
+        except FileNotFoundError:
+            logger.warning(f"Csv file for {material} does not exist.")
+            raise
+
+#        self.dataframe.absorption = self.dataframe.absorption.astype(float)
+
+    @property
+    def energies(self):
+        return self.dataframe.energy
+
+    @property
+    def dielectric_real(self):
+        return self.dataframe.real_part
+
+    @property
+    def dielectric_imag(self):
+        return self.dataframe.imaginary_part
+
+    @property
+    def absorption_coeff(self):
+        return self.dataframe.absorption
+
+    @property
+    def band_gap(self):
+        return band_gap[self.material]
+
+    @property
+    def reference(self):
+        return "THE HANDBOOK ON OPTICAL CONSTANTS OF SEMICONDUCTORS"
---
+++ vise/tests/analyzer/dielectric_function_data/test_exp_dielectric_func.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+from vise.analyzer.dielectric_function_data.exp_dielectric_func import \
+    ExpDieleFunc
+import numpy as np
+
+import pytest
+
+
+def test_gaas():
+    diele_gaas = ExpDieleFunc("GaAs")
+    assert diele_gaas.band_gap == 1.41
+    assert diele_gaas.energies[10] == 1.41
+    np.testing.assert_almost_equal(diele_gaas.dielectric_real[10], 13.16)
+    np.testing.assert_almost_equal(diele_gaas.dielectric_imag[10], 0.013)
+    np.testing.assert_almost_equal(diele_gaas.absorption_coeff[10], 2.58e+02)
+
+
+def test_file_not_found_error():
+    with pytest.raises(FileNotFoundError):
+        ExpDieleFunc("Au")
