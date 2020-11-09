b668ab9ceb556fcdf8ada4dd7725403de9681bc2
yuuukuma <yuuukuma@gmail.com>
2020-11-09 11:03:40 +0900

    Add effective_mass.py and its unittests

---
+++ vise/analyzer/effective_mass.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+from dataclasses import dataclass
+from typing import List
+import numpy as np
+
+from monty.json import MSONable
+from numpy import linalg
+from vise.util.mix_in import ToJsonFileMixIn
+
+
+@dataclass
+class EffectiveMass(MSONable, ToJsonFileMixIn):
+    p: np.ndarray  # [temperature][carrier concentration]
+    n: np.ndarray
+    temps: List[float]
+    concentrations: List[float]
+
+    def effective_mass(self, carrier_type, temp, concentration):
+        i_t = self.temps.index(temp)
+        i_c = self.concentrations.index(concentration)
+        return self.__getattribute__(carrier_type)[i_t][i_c]
+
+
+def eigvals_and_vecs(matrix: np.ndarray):
+    assert matrix.shape == (3, 3)
+    eigvals, eigvecs = linalg.eig(matrix)
+    idx = eigvals.argsort()
+    eigvals = eigvals[idx]
+    eigvecs = eigvecs[:, idx].T  # Need to transverse
+    return eigvals, eigvecs
+
+
+def lowest_eigval_and_vecs(matrix: np.ndarray):
+    eigvals, eigvecs = eigvals_and_vecs(matrix)
+    assert eigvals[0] > 0.0
+    eigval = eigvals[0]
+    lowest_eigvals = np.where(eigvals < eigval * 1.0001)
+    eigvecs = np.round(eigvecs[lowest_eigvals], 2)
+    return eigval, eigvecs
---
+++ vise/tests/analyzer/test_effective_mass.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+
+import numpy as np
+import pytest
+
+from monty.serialization import loadfn
+from vise.analyzer.effective_mass import EffectiveMass, eigvals_and_vecs, \
+    lowest_eigval_and_vecs
+
+
+@pytest.fixture
+def em():
+    return \
+        EffectiveMass(p=np.array([[[[1.0, 0, 0], [0, 1.0, 0], [0, 0, 1.0]]]]),
+                      n=np.array([[[[2.0, 0, 0], [0, 2.0, 0], [0, 0, 2.0]]]]),
+                      temps=[300],
+                      concentrations=[10**18])
+
+
+def test_effective_mass_json_file_mixin(em, tmpdir):
+    tmpdir.chdir()
+    em.to_json_file()
+    actual = loadfn("effective_mass.json")
+    assert (actual.p[0, 0] == em.p[0, 0]).all()
+
+
+def test_effective_mass(em):
+    actual = em.effective_mass("p", 300, 10**18)
+    assert (actual == em.p[0 ,0]).all()
+
+
+def test_lowest_eigval_and_vec(em):
+    matrix = np.array([[1.0, 0.2, 0.1], [0.2, 2.0, 0.3], [0.1, 0.3, 3.0]])
+    actual_vals, actual_vecs = eigvals_and_vecs(matrix)
+    expected_vals = [0.961, 1.945, 3.094]
+    vec = np.array([-46.194, 8.6, 1.0])
+    norm = np.linalg.norm(vec, ord=2)
+    expected_vec0 = vec / norm
+    np.testing.assert_array_almost_equal(actual_vals, expected_vals, decimal=3)
+    np.testing.assert_array_almost_equal(actual_vecs[0], expected_vec0)
+
+    actual_lowest_eigval, actual_eigvecs = lowest_eigval_and_vecs(matrix)
+    expected_lowest_eigval = 0.9606010675519356
+    expected_eigvecs = np.array([[-0.98,  0.18,  0.02]])
+    assert actual_lowest_eigval == expected_lowest_eigval
+    np.testing.assert_array_almost_equal(actual_eigvecs, expected_eigvecs)
+
+
+"""
+TODO
+-
+1.0  0.2  0.1
+0.2  2.0  0.3
+0.1  0.3  3.0
+DONE
+"""
