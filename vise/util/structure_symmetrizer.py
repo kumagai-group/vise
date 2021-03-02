8f5be0e05353f215c4784a848aa5524d94bf1bc6
yuuukuma <yuuukuma@gmail.com>
2021-03-03 08:43:20 +0900

    Move num_sym_op from num_symmetry_operation func

--- vise/util/structure_symmetrizer.py
+++ vise/util/structure_symmetrizer.py
@@ -295,6 +295,44 @@ class Site(MSONable):
         return " ".join(str_list)
 
 
+num_sym_op = {"1":     1,
+              "-1":    2,
+              "2":     2,
+              "m":     2,
+              "2/m":   4,
+              "222":   4,
+              "2mm":   4,
+              "m2m":   4,
+              "mm2":   4,
+              "mmm":   8,
+              "4":     4,
+              "-4":    4,
+              "4/m":   8,
+              "422":   8,
+              "4mm":   8,
+              "-4m2":  8,
+              "-42m":  8,
+              "4/mmm": 16,
+              "3":     3,
+              "-3":    6,
+              "32":    6,
+              "3m":    6,
+              "-3m":   12,
+              "6":     6,
+              "-6":    6,
+              "6/m":   12,
+              "622":   12,
+              "6mm":   12,
+              "-6m2":  12,
+              "6/mmm": 24,
+              "23":    12,
+              "m3":    24,
+              "m-3":   24,
+              "432":   24,
+              "-43m":  24,
+              "m-3m":  48}
+
+
 def num_symmetry_operation(point_group: str) -> int:
     """ Return number of symmetry operations from Hermann–Mauguin notation.
 
@@ -306,46 +344,9 @@ def num_symmetry_operation(point_group: str) -> int:
     Returns:
          Number of symmetry operation.
     """
-    d = {"1":     1,
-         "-1":    2,
-         "2":     2,
-         "m":     2,
-         "2/m":   4,
-         "222":   4,
-         "2mm":   4,
-         "m2m":   4,
-         "mm2":   4,
-         "mmm":   8,
-         "2/mmm": 8,
-         "4":     4,
-         "-4":    4,
-         "4/m":   8,
-         "422":   8,
-         "4mm":   8,
-         "-4m2":  8,
-         "-42m":  8,
-         "4/mmm": 16,
-         "3":     3,
-         "-3":    6,
-         "32":    6,
-         "3m":    6,
-         "-3m":   12,
-         "6":     6,
-         "-6":    6,
-         "6/m":   12,
-         "622":   12,
-         "6mm":   12,
-         "-6m2":  12,
-         "6/mmm": 24,
-         "23":    12,
-         "m3":    24,
-         "432":   24,
-         "-43m":  24,
-         "m-3m":  48}
-
     # remove "." from the given point_group
     point_group = "".join([s for s in point_group if s != "."])
-    return d[point_group]
+    return num_sym_op[point_group]
 
 
 class ViseSymmetryError(ViseError):
