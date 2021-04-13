cb471beab973a3ef23cd914f6535c5c2072d15ae
yuuukuma <yuuukuma@gmail.com>
2021-04-13 18:49:10 +0900

    Modify ToJsonFileMixIn to inherit ABC and add to_json abstractmethod

--- vise/util/mix_in.py
+++ vise/util/mix_in.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 import re
+from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Optional
 
 
-class ToJsonFileMixIn:
+class ToJsonFileMixIn(ABC):
     def to_json_file(self, filename: Optional[str] = None) -> None:
         filename = filename or self._filename
         Path(filename).write_text(self.to_json())
+
+    @abstractmethod
+    def to_json(self):
+        pass
 
     @property
     def _filename(self):
