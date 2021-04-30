ea44b9836f80a8a9af6024fa30dcc579b5c19a54
yuuukuma <yuuukuma@gmail.com>
2021-04-30 09:43:32 +0900

    Add make_spin_decomposed_volumetric_files and make_light_weight_vol_data and its unittests

--- vise/cli/main_util_functions.py
+++ vise/cli/main_util_functions.py
@@ -2,7 +2,13 @@
 #  Copyright (c) 2020. Distributed under the terms of the MIT License.
 
 from argparse import Namespace
+from pathlib import Path
 
+from pydefect.cli.vasp.make_defect_charge_info import make_spin_charges
+from pymatgen.io.vasp import Chgcar
+from vise.analyzer.vasp.handle_volumetric_data import \
+    light_weight_vol_text
+from vise.analyzer.vesta.vesta_file import add_density, VestaFile, calc_isurfs
 from vise.atom_energies.make_atom_vasp_set import make_atom_poscar_dirs
 from vise.util.logger import get_logger
 
@@ -13,3 +19,27 @@ def make_atom_poscars(args: Namespace) -> None:
     make_atom_poscar_dirs(args.dirname, args.elements)
 
 
+def make_spin_decomposed_volumetric_files(args):
+    chgcar = Chgcar.from_file(args.chgcar)
+    for c, spin in zip(make_spin_charges(chgcar), ["up", "down"]):
+        c.write_file(f"{args.chgcar}_{spin}")
+
+
+def make_light_weight_vol_data(args):
+    chgcar = Chgcar.from_file(args.volumetric_file)
+    output_vol_filename = (args.output_lw_volumetric_filename
+                           or Path(f"{args.volumetric_file}_lw"))
+    lw_vol_text = light_weight_vol_text(chgcar, args.border_fractions)
+    Path(output_vol_filename).write_text(lw_vol_text)
+
+    if args.output_vesta_filename:
+        is_chg = "CHG" in args.volumetric_file
+        volume = chgcar.structure.volume
+        isurfs = calc_isurfs(args.border_fractions, is_chg, volume)
+
+        if args.original_vesta_file:
+            vesta_text = args.original_vesta_file.read_text()
+        else:
+            vesta_text = VestaFile(chgcar.structure).__repr__()
+        to_vesta_text = add_density(vesta_text, isurfs, str(output_vol_filename))
+        Path(args.output_vesta_filename).write_text(to_vesta_text)
--- vise/tests/cli/test_main_util_functions.py
+++ vise/tests/cli/test_main_util_functions.py
@@ -4,14 +4,52 @@ from argparse import Namespace
 from pathlib import Path
 
 from pymatgen.core import Element
-from vise.cli.main_util_functions import make_atom_poscars
+from vise.cli.main_util_functions import make_atom_poscars, \
+    make_spin_decomposed_volumetric_files, make_light_weight_vol_data
+
+_file_path = "vise.cli.main_util_functions"
 
 
 def test_make_atom_poscars(mocker):
     args = Namespace(dirname=Path("a"), elements=[Element.H, Element.He])
-    mock = mocker.patch("vise.cli.main_utils_functions.make_atom_poscar_dirs")
+    mock = mocker.patch(f"{_file_path}.make_atom_poscar_dirs")
     make_atom_poscars(args)
     mock.assert_called_once_with(Path("a"), [Element.H, Element.He])
 
 
+def test_make_spin_decomposed_volumetric_files(mocker):
+    args = Namespace(chgcar="CHGCAR")
+    mock_chgcar = mocker.patch(f"{_file_path}.Chgcar")
+    mock_make_spin_charges = mocker.patch(f"{_file_path}.make_spin_charges")
+    chgcar = mock_chgcar.from_file.return_value
+    make_spin_decomposed_volumetric_files(args)
+    mock_chgcar.from_file.assert_called_once_with("CHGCAR")
+    mock_make_spin_charges.assert_called_once_with(chgcar)
 
+
+def test_make_light_weight_vol_data(mocker, tmpdir):
+    tmpdir.chdir()
+    Path("original.vesta").write_text("a")
+
+    args = Namespace(volumetric_file="CHGCAR",
+                     output_lw_volumetric_filename=Path("CHGCAR_lw"),
+                     border_fractions=[0.1],
+                     output_vesta_filename=Path("test_w_chg.vesta"),
+                     original_vesta_file=Path("original.vesta"))
+    mock_chgcar = mocker.patch(f"{_file_path}.Chgcar")
+    mock_vol_text = mocker.patch(f"{_file_path}.light_weight_vol_text")
+    mock_calc_isurfs = mocker.patch(f"{_file_path}.calc_isurfs")
+    mock_add_density = mocker.patch(f"{_file_path}.add_density")
+
+    chgcar = mock_chgcar.from_file.return_value
+    mock_add_density.return_value = "add_density_return"
+    mock_vol_text.return_value = "light_weight_vol_text"
+    mock_calc_isurfs.return_value = [0.0001]
+
+    make_light_weight_vol_data(args)
+    mock_vol_text.assert_called_once_with(chgcar, [0.1])
+    mock_calc_isurfs.assert_called_once_with([0.1], True,
+                                             chgcar.structure.volume)
+    mock_add_density.assert_called_once_with("a", [0.0001], "CHGCAR_lw")
+    assert Path("test_w_chg.vesta").read_text() == "add_density_return"
+    assert Path("CHGCAR_lw").read_text() == "light_weight_vol_text"
