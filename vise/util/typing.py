a78e5d4e545a1ed029849d34ee0c68eced7dcc48
yuuukuma <yuuukuma@gmail.com>
2021-05-03 16:10:41 +0900

    Add GenCoords

--- vise/util/typing.py
+++ vise/util/typing.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2021. Distributed under the terms of the MIT License.
-from typing import Tuple
+from typing import Tuple, List, Union
 
 Coords = Tuple[float, float, float]
+GenCoords = Union[Tuple[float, float, float], List[float]]
