4e2dd4d32cd2fd016fff45a9683f8096546beadd
yuuukuma <yuuukuma@gmail.com>
2021-05-30 12:06:56 +0900

    Add name_str_list to ExtendedEnum

--- vise/util/enum.py
+++ vise/util/enum.py
@@ -27,3 +27,7 @@ class ExtendedEnum(Enum):
     @classmethod
     def name_list(cls):
         return [e for e in cls]
+
+    @classmethod
+    def name_str_list(cls):
+        return [str(e) for e in cls]
