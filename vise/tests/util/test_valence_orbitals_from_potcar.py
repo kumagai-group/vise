a25af94dfb37a013429734daa3b068aef94c67f7
yuuukuma <yuuukuma@gmail.com>
2021-08-01 09:45:57 +0900

    Fix unittest

--- vise/tests/util/test_valence_orbitals_from_potcar.py
+++ vise/tests/util/test_valence_orbitals_from_potcar.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2021. Distributed under the terms of the MIT License.
 from pymatgen.io.vasp import Potcar
+from vise.input_set.potcar_generator import generate_potcar
+from vise.input_set.xc import Xc
 from vise.util.valence_orbitals_from_potcar import valence_orbitals_from_potcar
 
 
 def test_valence_orbitals_from_potcar():
-    potcar_single = Potcar(["Ba_sv"], "PBE")[0]
-    assert valence_orbitals_from_potcar(potcar_single) == "(6s)2 (5p)6 (5s)2"
+    potcar_single = generate_potcar(["Mg"], xc=Xc.pbe)[0]
+    assert valence_orbitals_from_potcar(potcar_single) == "(3s)2"
