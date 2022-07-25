ecae9be11096cfe7e545f5fc81b34edd8bce88f0
yuuukuma <yuuukuma@gmail.com>
2022-07-25 09:27:35 +0900

    refactor

--- vise/tests/input_set/test_vasp_input_files.py
+++ vise/tests/input_set/test_vasp_input_files.py
@@ -3,6 +3,7 @@
 
 from pathlib import Path
 
+import pytest
 from pymatgen.core.structure import Structure
 
 from vise.input_set.input_options import CategorizedInputOptions
@@ -11,16 +12,25 @@ from vise.input_set.vasp_input_files import VaspInputFiles
 from vise.input_set.xc import Xc
 
 
-def test_integration(tmpdir):
-    tmpdir.chdir()
+
+
+@pytest.fixture
+def vasp_input_files():
     lattice = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
     coords = [[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]]
     structure = Structure(lattice=lattice, species=["Mn", "O"], coords=coords)
     input_options = CategorizedInputOptions(
         structure=structure, task=Task.structure_opt, xc=Xc.pbe)
 
-    input_set = VaspInputFiles(input_options)
-    input_set.create_input_files(Path.cwd())
+    return VaspInputFiles(input_options)
+
+
+def test_integration(tmpdir, vasp_input_files):
+    tmpdir.chdir()
+    vasp_input_files.create_input_files(Path.cwd())
+
+
+#def test_vise_log():
 
 
 
