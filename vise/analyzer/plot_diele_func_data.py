0565000a2c0bddd59c2d73af54fa869aef11cc82
yuuukuma <yuuukuma@gmail.com>
2022-08-23 08:32:30 +0900

    add: TensorDirection

--- vise/analyzer/plot_diele_func_data.py
+++ vise/analyzer/plot_diele_func_data.py
@@ -3,12 +3,31 @@
 from math import log10
 from typing import List
 
+import numpy as np
 from matplotlib import pyplot as plt
 from plotly import graph_objects as go
 from vise.analyzer.dielectric_function import DieleFuncData
 from vise.analyzer.dielectric_function_data.exp_dielectric_func import \
     ExpDieleFunc
+from vise.util.enum import ExtendedEnum
 from vise.util.matplotlib import float_to_int_formatter
+
+
+class TensorDirection(ExtendedEnum):
+    average = "average"
+    xx = "xx"
+    yy = "yy"
+    zz = "zz"
+    xy = "xy"
+    yz = "yz"
+    xz = "xz"
+
+    def val(self, tensors: List[List[float]]) -> List[float]:
+        if self is self.average:
+            return [(t[0] + t[1] + t[2]) / 3 for t in tensors]
+        else:
+            idx = ["xx", "yy", "zz", "xy", "yz", "xz"].index(self.name)
+            return [tensor[idx] for tensor in tensors]
 
 
 class AbsorptionCoeffPlotter:
@@ -16,14 +35,12 @@ class AbsorptionCoeffPlotter:
     def __init__(self,
                  diele_func_data: DieleFuncData,
                  energy_range: List[float] = None,
-                 materials: List[str] = None,
                  yranges: List[float] = None):
 
+        self.diele_func_data = diele_func_data
         self.energies = diele_func_data.energies
-        self.absorption_coeff = diele_func_data.ave_absorption_coeff
         self.band_gap = diele_func_data.band_gap
         self.energy_range = energy_range or [0, 10]
-        self.materials = materials
 
         self._xaxis_title = "Energy (eV)"
 
@@ -39,18 +56,21 @@ class AbsorptionCoeffPlotlyPlotter(AbsorptionCoeffPlotter):
 
     _yaxis_title = "Absorption coefficient (cm <sup>-1</sup>)"
 
-    def create_figure(self):
+    def create_figure(self,
+                      directions=(TensorDirection.average,),
+                      materials: List[str] = None):
         fig = go.Figure()
-        fig.update_layout(
-            xaxis_title=self._xaxis_title,
-            yaxis_title=self._yaxis_title,
-            font_size=25,
-            width=800, height=700)
+        fig.update_layout(xaxis_title=self._xaxis_title,
+                          yaxis_title=self._yaxis_title,
+                          font_size=25, width=800, height=700)
 
-        fig.add_trace(go.Scatter(x=self.energies,
-                                 y=self.absorption_coeff,
-                                 line=dict(width=2.5),
-                                 name="Average"))
+        for direction in directions:
+            abs_coeff = direction.val(self.diele_func_data.absorption_coeff)
+
+            fig.add_trace(go.Scatter(x=self.energies,
+                                     y=abs_coeff,
+                                     line=dict(width=2.5),
+                                     name=direction.name))
         if self.band_gap:
             fig.add_trace(go.Scatter(x=[self.band_gap, self.band_gap],
                                      y=[self.ymin, self.ymax],
@@ -58,8 +78,8 @@ class AbsorptionCoeffPlotlyPlotter(AbsorptionCoeffPlotter):
                                      line_color="black",
                                      name="Band gap"))
 
-        if self.materials:
-            for material in self.materials:
+        if materials:
+            for material in materials:
                 exp = ExpDieleFunc(material)
                 energies = exp.energies
                 fig.add_trace(go.Scatter(x=energies, y=exp.absorption_coeff,
@@ -84,11 +104,12 @@ class AbsorptionCoeffPlotlyPlotter(AbsorptionCoeffPlotter):
 class AbsorptionCoeffMplPlotter(AbsorptionCoeffPlotter):
     _yaxis_title = "Absorption coefficient (cm$^{-1}$)"
 
-    def construct_plot(self):
-        self._add_coeffs()
+    def construct_plot(self, directions=(TensorDirection.average,),
+                       materials: List[str] = None):
+        self._add_coeffs(directions)
         self._add_band_gap()
-        if self.materials:
-            self._add_materials()
+        if materials:
+            self._add_materials(materials)
         # self._set_figure_legend()
         self._set_x_range()
         self._set_y_range()
@@ -96,15 +117,17 @@ class AbsorptionCoeffMplPlotter(AbsorptionCoeffPlotter):
         self._set_formatter()
         self.plt.tight_layout()
 
-    def _add_coeffs(self):
-        self.plt.semilogy(self.energies, self.absorption_coeff)
+    def _add_coeffs(self, directions):
+        for direction in directions:
+            abs_coeff = direction.val(self.diele_func_data.absorption_coeff)
+            self.plt.semilogy(self.energies, abs_coeff)
 
     def _add_band_gap(self):
         self.plt.axvline(x=self.band_gap, linestyle="dashed", color="black",
                          linewidth=1)
 
-    def _add_materials(self):
-        for material in self.materials:
+    def _add_materials(self, materials):
+        for material in materials:
             exp = ExpDieleFunc(material)
             energies = exp.energies
             self.plt.axvline(x=exp.band_gap,
--- vise/tests/analyzer/test_plot_diele_func_data.py
+++ vise/tests/analyzer/test_plot_diele_func_data.py
@@ -4,8 +4,8 @@ from copy import copy
 
 import pytest
 from pymatgen.io.vasp import Vasprun, Outcar
-from vise.analyzer.plot_absorption_coeff import AbsorptionCoeffPlotlyPlotter, \
-    AbsorptionCoeffMplPlotter
+from vise.analyzer.plot_diele_func_data import AbsorptionCoeffPlotlyPlotter, \
+    AbsorptionCoeffMplPlotter, TensorDirection
 
 from vise.analyzer.vasp.make_diele_func import make_diele_func
 from vise.util.dash_helper import show_png
@@ -16,6 +16,18 @@ try:
     PSUTIL_NOT_PRESENT = False
 except ModuleNotFoundError:
     PSUTIL_NOT_PRESENT = True
+
+
+def test_tensor_direction():
+    tensors = [[0.0, 1.0, 2.0, 3.0, 4.0, 5.0]]
+    actual = TensorDirection.average.val(tensors)
+    assert actual == [1.0]
+
+    actual = TensorDirection.xx.val(tensors)
+    assert actual == [0.0]
+
+    actual = TensorDirection.xy.val(tensors)
+    assert actual == [3.0]
 
 
 @pytest.fixture
@@ -29,14 +41,13 @@ def actual_diele_func_data(test_data_files):
 def test_absorption_coeff_plotly_plotter(actual_diele_func_data):
     data = copy(actual_diele_func_data)
     data.band_gap = None
-    plotter = AbsorptionCoeffPlotlyPlotter(data, materials=["GaAs"])
-    fig = plotter.create_figure()
+    plotter = AbsorptionCoeffPlotlyPlotter(data)
+    fig = plotter.create_figure(materials=["GaAs"])
     show_png(fig)
 
 
 def test_absorption_coeff_mpl_plotter(actual_diele_func_data):
-    plotter = AbsorptionCoeffMplPlotter(actual_diele_func_data,
-                                        materials=["GaAs", "Si"])
+    plotter = AbsorptionCoeffMplPlotter(actual_diele_func_data)
     #                                        align_gap=False)
-    plotter.construct_plot()
+    plotter.construct_plot(materials=["GaAs", "Si"])
     plotter.plt.show()
