05e308ad607e2103bc4a5fc411cb2e01b7da594a
yuuukuma <yuuukuma@gmail.com>
2022-10-21 15:45:36 +0900

    Modify BZPlotlyPlotter to make k1, k2, k3 plot as option

--- vise/analyzer/plot_brillouin_zone.py
+++ vise/analyzer/plot_brillouin_zone.py
@@ -5,12 +5,11 @@ from dataclasses import dataclass
 from math import sqrt
 from typing import List, Dict
 
-from monty.json import MSONable
-
-import plotly.graph_objects as go
-from vise.analyzer.plot_band_dos import plotly_sanitize_label
-from vise.util.plotly_util import sort_coords, make_triangles
 import numpy as np
+import plotly.graph_objects as go
+from monty.json import MSONable
+from vise.analyzer.plot_band_dos import plotly_sanitize_label
+from vise.util.plotly_util import sort_coords
 
 
 @dataclass
@@ -33,7 +32,7 @@ class BZPlotlyPlotter:
     def __init__(self, bz_plot_info: BZPlotInfo):
         self._bz_plot_info = bz_plot_info
 
-    def create_figure(self):
+    def create_figure(self, set_xyz_axes: bool = True, set_k123: bool = True):
         data = []
         for face_vertices in self._bz_plot_info.faces:
             vertex_coords = sort_coords(np.array(face_vertices))
@@ -58,10 +57,10 @@ class BZPlotlyPlotter:
                                      textposition="middle center",
                                      textfont=dict(size=32)))
 
-        for i, l in self._bz_plot_info.band_paths:
-            data.append(go.Scatter3d(x=[i[0], l[0]],
-                                     y=[i[1], l[1]],
-                                     z=[i[2], l[2]], mode="lines",
+        for lat_vec, l in self._bz_plot_info.band_paths:
+            data.append(go.Scatter3d(x=[lat_vec[0], l[0]],
+                                     y=[lat_vec[1], l[1]],
+                                     z=[lat_vec[2], l[2]], mode="lines",
                                      opacity=0.5,
                                      hoverinfo="none",
                                      line_width=8,
@@ -71,41 +70,52 @@ class BZPlotlyPlotter:
         c_max = _max * 1.3
         c_cone = _max * 0.3
 
-        data.append(go.Cone(x=[c_max], y=[0], z=[0], u=[c_cone], v=[0], w=[0],
-                            colorscale=[[0, 'rgb(200,200,200)'],
-                                        [1, 'rgb(200,200,200)']],
-                            showscale=False, hovertemplate="<b>x</b>"))
-        data.append(go.Cone(x=[0], y=[c_max], z=[0], u=[0], v=[c_cone], w=[0],
-                            colorscale=[[0, 'rgb(200,200,200)'],
-                                        [1, 'rgb(200,200,200)']],
-                            showscale=False, hovertemplate="<b>y</b>"))
-        data.append(go.Cone(x=[0], y=[0], z=[c_max], u=[0], v=[0], w=[c_cone],
-                            colorscale=[[0, 'rgb(200,200,200)'],
-                                        [1, 'rgb(200,200,200)']],
-                            showscale=False, hovertemplate="<b>z</b>"))
-        data.append(go.Scatter3d(x=[0, c_max], y=[0, 0], z=[0, 0], mode="lines",
-                                 hoverinfo="none",
-                                 marker_color="black"))
-        data.append(go.Scatter3d(x=[0, 0], y=[0, c_max], z=[0, 0], mode="lines",
-                                 hoverinfo="none",
-                                 marker_color="black"))
-        data.append(go.Scatter3d(x=[0, 0], y=[0, 0], z=[0, c_max], mode="lines",
-                                 hoverinfo="none",
-                                 marker_color="black"))
+        if set_xyz_axes:
+            data.extend([
+                go.Cone(x=[c_max], y=[0], z=[0], u=[c_cone], v=[0], w=[0],
+                        colorscale=[[0, 'rgb(200,200,200)'],
+                                    [1, 'rgb(200,200,200)']],
+                        showscale=False,
+                        hovertemplate="<b>k</b><sub>x</sub>"),
+                go.Cone(x=[0], y=[c_max], z=[0], u=[0], v=[c_cone], w=[0],
+                        colorscale=[[0, 'rgb(200,200,200)'],
+                                    [1, 'rgb(200,200,200)']],
+                        showscale=False,
+                        hovertemplate="<b>k</b><sub>y</sub>"),
+                go.Cone(x=[0], y=[0], z=[c_max], u=[0], v=[0], w=[c_cone],
+                        colorscale=[[0, 'rgb(200,200,200)'],
+                                    [1, 'rgb(200,200,200)']],
+                        showscale=False,
+                        hovertemplate="<b>k</b><sub>z</sub>"),
+                go.Scatter3d(x=[0, c_max], y=[0, 0], z=[0, 0], mode="lines",
+                             marker_color="black",
+                             hovertemplate="<b>k</b><sub>x</sub>"),
+                go.Scatter3d(x=[0, 0], y=[0, c_max], z=[0, 0], mode="lines",
+                             hoverinfo="none",
+                             marker_color="black",
+                             hovertemplate="<b>k</b><sub>y</sub>"),
+                go.Scatter3d(x=[0, 0], y=[0, 0], z=[0, c_max], mode="lines",
+                             hoverinfo="none",
+                             marker_color="black",
+                             hovertemplate="<b>k</b><sub>z</sub>")])
 
-        for i, direct in zip(self._bz_plot_info.rec_lat_vec, ["kx", "ky", "kz"]):
-            nn = sqrt(i[0] ** 2 + i[1] ** 2 + i[2] ** 2)
-            kx, ky, kz = np.array(i) * c_max / nn * 1.15
-            norm_kx, norm_ky, norm_kz = np.array(i) * c_cone / nn
-            data.append(go.Cone(x=[kx], y=[ky], z=[kz],
-                                u=[norm_kx], v=[norm_ky], w=[norm_kz],
-                                colorscale=[[0, 'rgb(100,100,100)'],
-                                            [1, 'rgb(100,100,100)']],
-                                showscale=False, hovertemplate=f"<b>{direct}</b>"))
-            data.append(go.Scatter3d(x=[0, kx], y=[0, ky], z=[0, kz],
-                                     mode="lines",
-                                     hoverinfo="none",
-                                     marker_color="black"))
+        if set_k123:
+            for direction, lat_vec in enumerate(self._bz_plot_info.rec_lat_vec):
+                nn = sqrt(lat_vec[0] ** 2 + lat_vec[1] ** 2 + lat_vec[2] ** 2)
+                kx, ky, kz = np.array(lat_vec) * c_max / nn * 1.15
+                norm_kx, norm_ky, norm_kz = np.array(lat_vec) * c_cone / nn
+                hovertemplate = f"<b>k</b><sub>{str(direction)}</sub>"
+                data.append(go.Cone(x=[kx], y=[ky], z=[kz],
+                                    u=[norm_kx], v=[norm_ky], w=[norm_kz],
+                                    colorscale=[[0, 'rgb(100,100,100)'],
+                                                [1, 'rgb(100,100,100)']],
+                                    showscale=False,
+                                    hovertemplate=hovertemplate))
+                data.append(go.Scatter3d(x=[0, kx], y=[0, ky], z=[0, kz],
+                                         mode="lines",
+                                         hoverinfo="none",
+                                         marker_color="black",
+                                         hovertemplate=hovertemplate))
 
         range_max = c_max * 1.4
         fig = go.Figure(data=data)
