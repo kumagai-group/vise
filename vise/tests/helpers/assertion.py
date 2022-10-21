80adb12136020da30e557ed3843bfdfe9a1e981c
yuuukuma <yuuukuma@gmail.com>
2022-10-21 16:46:48 +0900

    Modify assert_yaml_roundtrip

--- vise/tests/helpers/assertion.py
+++ vise/tests/helpers/assertion.py
@@ -80,6 +80,7 @@ def assert_yaml_roundtrip(obj: ToYamlFileMixIn,
         assert actual_text == expected_text
     except AssertionError:
         print(tmpdir)
+        print("actual text")
         print(actual_text)
         a = actual_text.split("\n")
         b = expected_text.split("\n")
