f896ab09894cfcdd17a32df3364ac667f730e9ab
yuuukuma <yuuukuma@gmail.com>
2022-11-04 18:20:18 +0900

    Modify to show ldau values in ViseLog

--- vise/input_set/vise_log.py
+++ vise/input_set/vise_log.py
@@ -2,7 +2,7 @@
 #  Copyright (c) 2022. Distributed under the terms of the MIT License.
 from copy import copy
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Dict
 
 import yaml
 from monty.json import MSONable
@@ -18,6 +18,8 @@ class ViseLog(MSONable, ToYamlFileMixIn):
     xc: Xc
     input_options: dict
     user_incar_settings: Optional[dict] = None
+    ldauu: Optional[Dict[str, float]] = None
+    ldaul: Optional[Dict[str, float]] = None
 
     def to_yaml(self):
         return yaml.safe_dump(self.as_dict(), sort_keys=False)
@@ -27,6 +29,10 @@ class ViseLog(MSONable, ToYamlFileMixIn):
              "xc": str(self.xc), "input_options": self.input_options}
         if self.user_incar_settings is not None:
             d["user_incar_settings"] = self.user_incar_settings
+        if self.ldauu is not None:
+            d["ldauu"] = self.ldauu
+        if self.ldaul is not None:
+            d["ldaul"] = self.ldaul
         return d
 
     @classmethod
--- vise/tests/input_set/test_vise_log.py
+++ vise/tests/input_set/test_vise_log.py
@@ -13,7 +13,8 @@ from vise.tests.helpers.assertion import assert_yaml_roundtrip
 @pytest.fixture
 def vise_log():
     return ViseLog(version="0.6.3", task=Task.structure_opt, xc=Xc.pbe,
-                   input_options={"charge": 1}, user_incar_settings={"NSW": 2})
+                   input_options={"charge": 1}, user_incar_settings={"NSW": 2},
+                   ldauu={"Mg": 3}, ldaul={"Mg": 4})
 
 
 def test_vise_log(vise_log, tmpdir):
@@ -24,6 +25,10 @@ input_options:
   charge: 1
 user_incar_settings:
   NSW: 2
+ldauu:
+  Mg: 3
+ldaul:
+  Mg: 4
 """
     assert_yaml_roundtrip(vise_log, tmpdir, expected_text)
 
