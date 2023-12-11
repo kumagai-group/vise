819733b7f26c5bd56be01d81b761ec8706906bde
yuuukuma <yuuukuma@gmail.com>
2023-12-12 07:25:28 +0900

    add unittest

---
+++ vise/tests/util/test_enum.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2020. Distributed under the terms of the MIT License.
+from dataclasses import dataclass
+from pathlib import Path
+
+from vise.util.enum import ExtendedEnum
+
+
+def test_extended_enum():
+
+    @dataclass
+    class Test(ExtendedEnum):
+        a: str = "a"
+
+    assert Test("a") == Test.a
+
+
