7afcdfb23f272fa73edfa61d3384f647b4706b38
yuuukuma <yuuukuma@gmail.com>
2024-03-05 08:02:41 +0900

    modify to use name instead of value for string in ExtendedEnum

--- vise/util/enum.py
+++ vise/util/enum.py
@@ -8,15 +8,15 @@ from enum import unique, Enum
 class ExtendedEnum(Enum):
 
     def __repr__(self):
-        return self.value
+        return self.name
 
     def __str__(self):
-        return self.value
+        return self.name
 
     @classmethod
     def from_string(cls, s: str):
         for m in cls:
-            if m.value == s:
+            if m.name == s:
                 return m
         raise AttributeError(f"{str(s)} is not proper member for {cls}.\n",
                              f"Supported names:\n {cls.names_string()}")
